ca5a9f35708b6c98cfc813faf4c5373dabd9d974
Ziad Abdelrahman <justziadali@gmail.com>
2024-09-30 16:19:50 +0300

    refactor: removed unnecessary vite data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -47,15 +47,29 @@ function TouristAttractions() {
 
 function App() {
   return (
-    <Router>
+    <>
       <div>
-        <Routes>
-          <Route path="/" element={<TouristAttractions />} />
-          <Route path="/non-tourist" element={<NonTouristView />} />
-        </Routes>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </Router>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App;
