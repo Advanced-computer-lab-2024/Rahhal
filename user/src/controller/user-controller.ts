4d3680f2aa90dc62a01dd7332b8873e8b2a13505
YousefElbrolosy <yousefelbrolosy@gmail.com>
2024-09-30 15:21:08 +0300

    feat: validation for create-delete done as well as modified user-model validation and required

--- user/src/controller/user-controller.ts
+++ user/src/controller/user-controller.ts
@@ -49,4 +49,30 @@ const updateUserByUsername = async (req: Request, res: Response) => {
     }
 };
 
-export default {getUserByUsername, getAllUsers, updateUserByUsername};
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const userData = req.body;
+    const user = await userService.createUser(userData);
+    res.status(200).json({"data":user});
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({"error": error.message});
+    }
+  }
+};
+
+const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.id;
+    const user = await userService.deleteUser(userId);
+    res.status(200).json({"data":user});
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({"error": error.message});
+    }
+  }
+};
+
+export default {getUserByUsername, getAllUsers, updateUserByUsername, createUser, deleteUser}
+
--- user/src/repository/user-repository.ts
+++ user/src/repository/user-repository.ts
@@ -2,9 +2,19 @@ import  user , {IUser} from '../model/user-model';
 
 
 //get specific user
-export async function getUserByUsername(username : string) : Promise<IUser | null> {
-    return await user.findOne({ userName : username });
-};
+export async function getUserByUsername(
+  username: string,
+): Promise<IUser | null> {
+  return await User.findOne({ username: username });
+}
+
+export async function getUserByEmail(email: string): Promise<IUser | null> {
+  return await User.findOne({ email: email });
+}
+
+export async function getUserById(userId: string): Promise<IUser | null> {
+  return await User.findOne({ _id: userId });
+}
 
 //get all users
 export async function getAllUsers () : Promise<IUser[] | null> {
@@ -12,6 +22,20 @@ export async function getAllUsers () : Promise<IUser[] | null> {
 };
 
 //update user
-export async function updateUserByUsername (username : string, updatedUser : IUser) : Promise<IUser | null> {
-    return await user.findOneAndUpdate({ username : username }, updatedUser, { new : true });
-};
+export async function updateUserByUsername(
+  username: string,
+  updatedUser: IUser,
+): Promise<IUser | null> {
+  return await User.findOneAndUpdate({ username: username }, updatedUser, {
+    new: true,
+  });
+}
+
+export async function createUser(userData: IUser) {
+  const newUser = new User(userData);
+  return await newUser.save();
+}
+
+export async function deleteUser(userId: string) {
+  return await User.findByIdAndDelete(userId);
+}
