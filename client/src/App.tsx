c60588dcf1e5db6bf3987ff5304bce067bf7b097
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-09-30 16:21:20 +0300

    feat(client): create generic card component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
+import "./components/nanna/EntertainmentCard.css";
+import EntertainmentCard from "./components/nanna/EntertainmentCard";
+import Pyramid from "./assets/pyramids.webp";
+import felluca from "./assets/aswan2.webp";
+import ski from "./assets/ski egypt.jpg";
+
+import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
+    <div style={{display:"flex", flexDirection:"row", gap:"10px"}}>
+    <div>
+    <EntertainmentCard image={Pyramid} rating={5} title={"Great Pyramids Of Giza"} location="Giza" price={200} openingTime="Sunday-Saturday 7AM-5PM"/>
+    </div>
+
+    <div>
+      <EntertainmentCard image={ski} rating={4.8} title={"Ski Egypt"} location={"6th Of October"} price={200} availability={true}/>
+    </div>
+    <div>
+      <EntertainmentCard image={felluca} rating={4.3} title={"Felluca Ride in Nile Aswan"} location="Aswan" price={300} language={["English","Arabic","Italian"]}/>
+    </div>
+    </div>
+  )
 }
 
 export default App;
---
+++ client/src/components/nanna/EntertainmentCard.tsx
@@ -0,0 +1,65 @@
+import "./EntertainmentCard.css";
+import { IoMdStar } from "react-icons/io";
+import { FaLocationDot } from "react-icons/fa6";
+import { GrLanguage } from "react-icons/gr";
+interface EntertainmentCardProps {
+    image: string;
+    title: string;
+    price?: number;
+    rating: number;
+    location?: string;
+    language?: string[];
+    openingTime?: string;
+    availability?:boolean;
+}
+
+const EntertainmentCard: React.FC<EntertainmentCardProps> = ({image, title, price, rating, location, language, openingTime, availability}) => {
+    return(
+        <>
+        <div className="entertainment-card-container">
+
+            <div className="entertainment-card-container__image">
+                {/* image and bookmark goes here */}
+                <img src={image} alt="entertainment"/>
+            </div>
+
+            <div className="entertainment-card-container__details">
+
+                <div className="entertainment-card-container__title-location">
+                    {/* title and location goes here */}
+                    <h3>{title}</h3>
+                    {location!==undefined && ( <p> <FaLocationDot style={{ fontSize: "0.8rem" }}/> {location}</p>)}
+                </div>
+                <div className="entertainment-card-container__rating">
+                    {/* rating goes here */}
+                    <IoMdStar style={{ fontSize: "0.8rem" }} />
+                    <p>{rating?.toPrecision(2) }</p>
+                    
+                </div>
+                <div className="entertainment-card-container__language-time">
+                    {/* language and opening time and availability goes here */}
+
+                   {language!==undefined && ( <p><br/><GrLanguage /> {language?.join('/')}</p>)}
+
+                   {openingTime!==undefined && ( <p>Opening Hours: <br/> {openingTime}</p>)}
+
+                    {availability!==undefined && (
+                    availability? <p><br/>Available Book NOW!</p> : <p><br/>Booking Closed!</p>)}
+                </div>
+
+                <div className="entertainment-card-container__price">
+                    {/* price goes here */}
+                    <h5>From {price} EGP</h5>
+                </div>
+                
+
+            </div>
+
+        </div>
+        </>
+    )
+}
+
+export default EntertainmentCard;
+
+
