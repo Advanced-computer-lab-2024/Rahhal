c2b555d37cd73fba0651cb9c89b447521baab891
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-09-30 14:20:16 +0300

    feat: add update and delete preference tag endpoints

--- entertainment/src/services/preference-tags-service.ts
+++ entertainment/src/services/preference-tags-service.ts
@@ -1,13 +1,22 @@
 import * as preferenceTagsRepository from '../repositories/preference-tags-repository';
 
-export const getPreferenceTags = async () => {
-    return await preferenceTagsRepository.getPreferenceTags();
+export const getPreferenceTags= async ()=>{ 
+  return await preferenceTagsRepository.getPreferenceTags();
 }
 
-export const getPreferenceTag = async (id: string) => {
-    return await preferenceTagsRepository.getPreferenceTag(id);
+export const getPreferenceTag= async (id: string) => {
+  return await preferenceTagsRepository.getPreferenceTag(id);
 }
 
-export const createPreferenceTag = async (name: string) => {
-    return await preferenceTagsRepository.createPreferenceTag(name);
+export const createPreferenceTag= async (name: string) => {
+  return await preferenceTagsRepository.createPreferenceTag(name);
 }
+
+export const updatePreferenceTag = async (id: string, name:string) => {
+  return await preferenceTagsRepository.updatePreferenceTag(id, name);
+}
+
+export const deletePreferenceTag = async (id: string) => {
+  return await preferenceTagsRepository.deletePreferenceTag(id);
+}
+
