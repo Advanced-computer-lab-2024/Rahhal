0943de272939e82dafd1e6ed5376e3820cf4fe0e
Abdullah Mahmoud <abdullahmahmoud289@gmail.com>
2024-09-30 22:54:01 +0300

    feat: add get and create tags endpoints

--- entertainment/src/routes/preference-tags-routes.ts
+++ entertainment/src/routes/preference-tags-routes.ts
@@ -1,1 +1,10 @@
+import express from 'express';
+import preferenceTagController from '../controllers/preference-tags-controller';
 
+const router = express.Router();
+
+router.get('/', preferenceTagController.getPreferenceTags);
+router.get('/:id', preferenceTagController.getPreferenceTag);
+router.post('/', preferenceTagController.createPreferenceTag);
+
+export default router;
