47e1c12c43a63384016d39f41c90a9fcd65b4f6b
Ziad Abdelrahman <justziadali@gmail.com>
2024-10-02 01:16:26 +0300

    shadcn components & some refactoring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,69 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import EntertainmentCard from "@/components/entertainment-card/EntertainmentCard";
-import Pyramid from "./assets/pyramids.webp";
-import felluca from "./assets/aswan2.webp";
-import ski from "./assets/ski egypt.jpg";
-import NonTouristView from "@/components/non-tourist/NonTouristView";
+import SearchBar from './components/shadcn/SearchBar.tsx'
 
-import "./App.css";
-
-function TouristAttractions() {
-  return (
-    <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
-      <div>
-        <EntertainmentCard
-          image={Pyramid}
-          rating={5}
-          title={"Great Pyramids Of Giza"}
-          location="Giza"
-          price={200}
-          openingTime="Sunday-Saturday 7AM-5PM"
-        />
-      </div>
-
-      <div>
-        <EntertainmentCard
-          image={ski}
-          rating={4.8}
-          title={"Ski Egypt"}
-          location={"6th Of October"}
-          price={200}
-          availability={true}
-        />
-      </div>
-      <div>
-        <EntertainmentCard
-          image={felluca}
-          rating={4.3}
-          title={"Felluca Ride in Nile Aswan"}
-          location="Aswan"
-          price={300}
-          language={["English", "Arabic", "Italian"]}
-        />
-      </div>
-    </div>
-  );
-}
-
-function App() {
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div className="flex items-center justify-center h-screen">
+      <SearchBar/>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
