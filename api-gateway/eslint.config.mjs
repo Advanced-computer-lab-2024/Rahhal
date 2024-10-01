809f4ca8b76d3695e1208e191a15729181112cad
Ahmed Hawater <hawatercoding@gmail.com>
2024-10-01 22:06:31 +0300

    style: Update eslint and tsconfig rule
    - Prevent magic numbers and enforce array types T[] to ensure code consistency
    
    - Use and prefer absolute rather than relative path in imports

--- api-gateway/eslint.config.mjs
+++ api-gateway/eslint.config.mjs
@@ -1,7 +1,12 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.strict,
-);
+export default tseslint.config({
+  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+  rules: {
+    "no-magic-numbers": "off",
+    "@typescript-eslint/no-magic-numbers": "error",
+    "@typescript-eslint/array-type": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
+  },
+});
