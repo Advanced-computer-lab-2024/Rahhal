2c50cdc9856b97b2094f48017c332f6321c66903
Ziad Abdelrahman <justziadali@gmail.com>
2024-10-02 03:48:44 +0300

    feat: name validation

--- entertainment/src/models/Category.ts
+++ entertainment/src/models/Category.ts
@@ -3,10 +3,15 @@ import mongoose from 'mongoose';
 const CategorySchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     unique: true,
     lowercase: true,
-  },
+    required:[true, 'Name is required'],
+    validate: {
+      validator: (name: string) => {
+        return typeof name === 'string' && name.trim().length > 0;
+      }
+    },
+  }
 });
 
 const Category = mongoose.model('Category', CategorySchema);
