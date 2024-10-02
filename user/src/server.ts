d52319d8b580a348b995f4bad8fcca7a1a5528d0
Ali Shokry <amsaar.1416@gmail.com>
2024-10-02 20:21:25 +0300

    refactor: adjust server file to take port number from .env

--- user/src/server.ts
+++ user/src/server.ts
@@ -1,5 +1,10 @@
 import app from './app';
-const PORT = process.env.PORT || 3000;
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT;
+if (!PORT) {
+  throw new Error('Port is not defined in .env file');
+}
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
