9769406b190ae76ec8d284c7931f4ac648612f2d
Amr Hegazy <68474553+Amr-Hegazy1@users.noreply.github.com>
2024-10-02 13:14:10 +0300

    feat: Finished CRUDs for activities (#2)
    * feat: Finished CRUDs for activities
    
    - Instantiated backend architecture
    
    - Updated `package.json' to run `server.ts` instead of 'app.ts`
    
    - Added routes
    
    * added swagger
    
    * fix: - Fixed API activites routing
    - Exported status codes in `utils/constants.ts` to improve maintainablity and reradablity
    - Changed file `activity-model.ts` to `Activity,ts`
    
    * fix: - Refactor routes to use activities-router instead of activities-routes
    - Added environemnt file support
    - Added min and max rating magic numbers as constants ro `utils/constants.ts`
    - Updated validator function parameter names to be more readable and maintainable
    - switched from `exports.*` to `export default *`
    
    * refactor: Remove unnecessary console.log statement
    
    * refactor: Update Dockerfile and activities-repository.ts
    
    - Update Dockerfile to include npm install command
    - Update activities-repository.ts to run validators when updating an activity
    
    * refactor: Update location property in Activity model to use longitude and latitude objects instead of tuple of numbers
    
    * refactor: Divide code into api, database and services layers
    
    * feat: Add swagger api docs endpoint
    
    * chore: Add swagger-ui and swagger-autogen deps
    
    * docs: Add .env.example file
    
    * fix: Resolve package.json and package-lock.json conflicts
    
    ---------
    
    Co-authored-by: Ahmed Hawater <hawatercoding@gmail.com>

---
+++ entertainment/src/api/controllers/activities-controller.ts
@@ -0,0 +1,99 @@
+import express from "express";
+import * as activitiesService from "../../services/activities-service";
+import { STATUS_CODES } from "../../utils/constants";
+
+export async function getAllActivities(
+  req: express.Request,
+  res: express.Response,
+) {
+  try {
+    const activities = await activitiesService.getAllActivities();
+    res.status(STATUS_CODES.STATUS_OK).json(activities);
+  } catch (error: unknown) {
+    res.status(STATUS_CODES.SERVER_ERROR).json({
+      message:
+        error instanceof Error ? error.message : "An unknown error occurred",
+    });
+  }
+}
+
+export async function getActivityById(
+  req: express.Request,
+  res: express.Response,
+) {
+  try {
+    const activity = await activitiesService.getActivityById(req.params.id);
+    if (!activity) {
+      res
+        .status(STATUS_CODES.NOT_FOUND)
+        .json({ message: "Activity not found" });
+    } else {
+      res.status(STATUS_CODES.STATUS_OK).json(activity);
+    }
+  } catch (error: unknown) {
+    res.status(STATUS_CODES.SERVER_ERROR).json({
+      message:
+        error instanceof Error ? error.message : "An unknown error occurred",
+    });
+  }
+}
+
+export async function createActivity(
+  req: express.Request,
+  res: express.Response,
+) {
+  try {
+    const activity = await activitiesService.createActivity(req.body);
+    res.status(STATUS_CODES.CREATED).json(activity);
+  } catch (error: unknown) {
+    res.status(STATUS_CODES.SERVER_ERROR).json({
+      message:
+        error instanceof Error ? error.message : "An unknown error occurred",
+    });
+  }
+}
+
+export async function updateActivity(
+  req: express.Request,
+  res: express.Response,
+) {
+  try {
+    const activity = await activitiesService.updateActivity(
+      req.params.id,
+      req.body,
+    );
+    if (!activity) {
+      res
+        .status(STATUS_CODES.NOT_FOUND)
+        .json({ message: "Activity not found" });
+    } else {
+      res.status(STATUS_CODES.STATUS_OK).json(activity);
+    }
+  } catch (error: unknown) {
+    res.status(STATUS_CODES.SERVER_ERROR).json({
+      message:
+        error instanceof Error ? error.message : "An unknown error occurred",
+    });
+  }
+}
+
+export async function deleteActivity(
+  req: express.Request,
+  res: express.Response,
+) {
+  try {
+    const activity = await activitiesService.deleteActivity(req.params.id);
+    if (!activity) {
+      res
+        .status(STATUS_CODES.NOT_FOUND)
+        .json({ message: "Activity not found" });
+    } else {
+      res.status(STATUS_CODES.STATUS_OK).json({ message: "Activity deleted" });
+    }
+  } catch (error: unknown) {
+    res.status(STATUS_CODES.SERVER_ERROR).json({
+      message:
+        error instanceof Error ? error.message : "An unknown error occurred",
+    });
+  }
+}
---
+++ entertainment/src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import activitiesRoutes from "./api/routes/activities-routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./api/swagger/swagger-output.json";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/activities", activitiesRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+export default app;
