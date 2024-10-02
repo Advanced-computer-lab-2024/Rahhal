f82167d6374bb433a618687124362f73beed67c8
YousefElbrolosy <yousefelbrolosy@gmail.com>
2024-10-02 14:08:53 +0300

    fix(user-controller): multiple delete requests of a not found user now does not yield a ERR_HTTP_HEADERS_SENT

--- user/src/api/controllers/user-controller.ts
+++ user/src/api/controllers/user-controller.ts
@@ -1,103 +1,96 @@
-import {Request , Response}  from "express";
+import { Request, Response } from "express";
 import * as userService from "../../services/user-service";
-import {STATUS_CODES} from "../../utils/constants";
+import { STATUS_CODES } from "../../utils/constants";
 
 const getUserByUsername = async (req: Request, res: Response) => {
-    try {
-        const username = req.body.username;
-        const user = await userService.getUserByUsername(username);
-        if (!user) {
-             res.status(STATUS_CODES.NOT_FOUND).json({message : "User not found"});
-        }
-        else{
-            res.status(STATUS_CODES.STATUS_OK).json(user);
-        }
-    } catch (error) {
-         res.status(STATUS_CODES.SERVER_ERROR).send(error);
+  try {
+    const username = req.body.username;
+    const user = await userService.getUserByUsername(username);
+    if (!user) {
+      res.status(STATUS_CODES.NOT_FOUND).json({ message: "User not found" });
+    } else {
+      res.status(STATUS_CODES.STATUS_OK).json(user);
     }
+  } catch (error) {
+    res.status(STATUS_CODES.SERVER_ERROR).send(error);
+  }
 };
 
 //get all users
 const getAllUsers = async (req: Request, res: Response) => {
-    try {
-        const users = await userService.getAllUsers();
-        if (!users) {
-            res.status(STATUS_CODES.NOT_FOUND).json({message : "No users found"});
-        }
-        else{
-            res.status(STATUS_CODES.STATUS_OK).json(users);
-        }
-    } catch (error) {
-        res.status(STATUS_CODES.SERVER_ERROR).send(error);
+  try {
+    const users = await userService.getAllUsers();
+    if (!users) {
+      res.status(STATUS_CODES.NOT_FOUND).json({ message: "No users found" });
+    } else {
+      res.status(STATUS_CODES.STATUS_OK).json(users);
     }
+  } catch (error) {
+    res.status(STATUS_CODES.SERVER_ERROR).send(error);
+  }
 };
 
 //getUserById
 const getUserById = async (req: Request, res: Response) => {
-    try {
-        const userId = req.params.id;
-        const user = await userService.getUserById(userId);
-        if (!user) {
-            res.status(STATUS_CODES.NOT_FOUND).json({message : "User not found"});
-        }
-        else{
-            res.status(STATUS_CODES.STATUS_OK).json(user);
-        }
-    } catch (error) {
-        res.status(STATUS_CODES.SERVER_ERROR).send(error);
+  try {
+    const userId = req.params.id;
+    const user = await userService.getUserById(userId);
+    if (!user) {
+      res.status(STATUS_CODES.NOT_FOUND).json({ message: "User not found" });
+    } else {
+      res.status(STATUS_CODES.STATUS_OK).json(user);
     }
+  } catch (error) {
+    res.status(STATUS_CODES.SERVER_ERROR).send(error);
+  }
 };
 
 //get User by email
 const getUserByEmail = async (req: Request, res: Response) => {
-    try {
-        const email = req.body.email;
-        const user = await userService.getUserByEmail(email);
-        if (!user) {
-            res.status(STATUS_CODES.NOT_FOUND).json({message : "User not found"});
-        }
-        else{
-            res.status(STATUS_CODES.STATUS_OK).json(user);
-        }
-    } catch (error) {
-        res.status(STATUS_CODES.SERVER_ERROR).send(error);
+  try {
+    const email = req.body.email;
+    const user = await userService.getUserByEmail(email);
+    if (!user) {
+      res.status(STATUS_CODES.NOT_FOUND).json({ message: "User not found" });
+    } else {
+      res.status(STATUS_CODES.STATUS_OK).json(user);
     }
+  } catch (error) {
+    res.status(STATUS_CODES.SERVER_ERROR).send(error);
+  }
 };
 
 //update user by username
 const updateUserByUsername = async (req: Request, res: Response) => {
-    try {
-        const username = req.body.username;
-        const updatedUser = req.body;
-        const user = await userService.updateUserByUsername(username, updatedUser);
-        if (!user) {
-            res.status(STATUS_CODES.NOT_FOUND).json({message : "User not found"});
-        }
-        else{
-            res.status(STATUS_CODES.STATUS_OK).json(user);
-        }
-    } catch (error) {
-        res.status(STATUS_CODES.SERVER_ERROR).send(error);
+  try {
+    const username = req.body.username;
+    const updatedUser = req.body;
+    const user = await userService.updateUserByUsername(username, updatedUser);
+    if (!user) {
+      res.status(STATUS_CODES.NOT_FOUND).json({ message: "User not found" });
+    } else {
+      res.status(STATUS_CODES.STATUS_OK).json(user);
     }
+  } catch (error) {
+    res.status(STATUS_CODES.SERVER_ERROR).send(error);
+  }
 };
 
 //update user by userId
 const updateUserById = async (req: Request, res: Response) => {
-    try {
-        const userId = req.params.id;
-        const updatedUser = req.body;
-        const user = await userService.updateUserById(userId, updatedUser);
-        if (!user) {
-            res.status(STATUS_CODES.NOT_FOUND).json({message : "User not found"});
-        }
-        else{
-            res.status(STATUS_CODES.STATUS_OK).json(user);
-        }
-    } catch (error) {
-        res.status(STATUS_CODES.SERVER_ERROR).send(error);
+  try {
+    const userId = req.params.id;
+    const updatedUser = req.body;
+    const user = await userService.updateUserById(userId, updatedUser);
+    if (!user) {
+      res.status(STATUS_CODES.NOT_FOUND).json({ message: "User not found" });
+    } else {
+      res.status(STATUS_CODES.STATUS_OK).json(user);
     }
+  } catch (error) {
+    res.status(STATUS_CODES.SERVER_ERROR).send(error);
+  }
 };
-
 
 const createUser = async (req: Request, res: Response) => {
   try {
@@ -106,7 +99,7 @@ const createUser = async (req: Request, res: Response) => {
     res.status(STATUS_CODES.STATUS_OK).json(user);
   } catch (error) {
     if (error instanceof Error) {
-      res.status(400).json({"error": error.message});
+      res.status(400).json({ error: error.message });
     }
   }
 };
@@ -115,17 +108,26 @@ const deleteUser = async (req: Request, res: Response) => {
   try {
     const userId = req.params.id;
     const user = await userService.getUserById(userId);
-    if(!user){
-        res.status(STATUS_CODES.NOT_FOUND).json({message : "User not found"});
+    if (!user) {
+      res.status(STATUS_CODES.NOT_FOUND).json({ message: "User not found" });
+    } else {
+      const deletedUser = await userService.deleteUser(userId);
+      res.status(STATUS_CODES.STATUS_OK).json(deletedUser);
     }
-    const deletedUser = await userService.deleteUser(userId);
-    res.status(STATUS_CODES.STATUS_OK).json(deletedUser);
   } catch (error) {
     if (error instanceof Error) {
-      res.status(400).json({"error": error.message});
+      res.status(400).json({ error: error.message });
     }
   }
 };
 
-export default {getUserByUsername, getAllUsers, updateUserByUsername, createUser, deleteUser , getUserById , updateUserById , getUserByEmail};
-
+export default {
+  getUserByUsername,
+  getAllUsers,
+  updateUserByUsername,
+  createUser,
+  deleteUser,
+  getUserById,
+  updateUserById,
+  getUserByEmail,
+};
