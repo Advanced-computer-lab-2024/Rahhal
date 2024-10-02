e7a52e9c64c864e23096085c2595fa4129d180dd
Ziad Abdelrahman <justziadali@gmail.com>
2024-10-02 03:48:10 +0300

    added server & app

--- entertainment/src/app.ts
+++ entertainment/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import activitiesRoutes from "./api/routes/activities-routes";
 import ItinerariesRoutes from "./api/routes/itineraries-routes";
 import preferenceTagRoutes from "./api/routes/preference-tags-routes";
+import categoryRouter from './routes/category-router';
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./api/swagger/swagger-output.json";
 
@@ -13,6 +14,7 @@ app.use(express.json());
 // Routes
 app.use("/activities", activitiesRoutes);
 app.use("/itenaries", ItinerariesRoutes);
+app.use('/categories',categoryRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/preference-tags", preferenceTagRoutes);
 export default app;
