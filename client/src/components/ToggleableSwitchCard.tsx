e6f85a4703f332f201953d36d59f74d06390837b
Amr Hegazy <68474553+Amr-Hegazy1@users.noreply.github.com>
2024-10-02 22:56:32 +0300

    Client feat/modal toggle switch (#6)
    * feat: - Integrated shadcn into file structure
    - Added `ToggleableSwitchCard` component
    
    * refactor: changed component name in ToggleableSwitchCard.tsx
    
    * refactor: update text alignment in ToggleableSwitchCard.tsx to be left aligned

---
+++ client/src/components/ToggleableSwitchCard.tsx
@@ -0,0 +1,41 @@
+import { Switch } from "@/components/ui/switch"
+import {
+
+  CardContent,
+} from "@/components/ui/card"
+
+import React from "react"
+
+
+
+interface ToggleableSwitchCardProps {
+  title: string
+  description: string
+  icon: React.ReactNode
+}
+export const ToggleableSwitchCard = ({ title, description, icon }: ToggleableSwitchCardProps) => {
+  return (
+    
+    
+      <CardContent className="grid gap-4">
+        <div className=" flex items-center space-x-4 rounded-md border p-4">
+              {icon}
+              <div className="flex-1 space-y-1">
+                <p className="text-sm text-left font-medium leading-none">
+                  {title}
+                </p>
+                <p className="text-sm text-left text-muted-foreground">
+                {description}
+            </p>
+          </div>
+          <Switch />
+        </div>
+        </CardContent>
+        
+      
+    
+    
+  
+  )
+
+}
