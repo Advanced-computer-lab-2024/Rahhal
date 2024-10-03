23252061aea9d7689601759461dd70dcfa56ff5e
ahdmostafa0 <ahdmostafa327@gmail.com>
2024-10-03 20:45:52 +0300

    refactor: Move db validating functions into a seprate folder

--- entertainment/src/database/models/Itinerary.ts
+++ entertainment/src/database/models/Itinerary.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { CONSTANTS } from "../../utils/constants";
+import * as itineraryValidator from "../validators/itineraries-validator";
 
 export interface IItinerary {
   name: string;
@@ -31,7 +31,7 @@ const itinerarySchema = new mongoose.Schema<IItinerary>({
     ],
     required: true,
     validate: [
-      validateLocations,
+      itineraryValidator.validateLocations,
       "Invalid location coordinates: Longitude must be between -180 and 180, latitude must be between -90 and 90",
     ],
   },
@@ -42,7 +42,7 @@ const itinerarySchema = new mongoose.Schema<IItinerary>({
     type: mongoose.Schema.Types.Mixed,
     required: true,
     validate: {
-      validator: validateTour_price,
+      validator: itineraryValidator.validateTourPrice,
       message: "Invalid price format, must be a number or an object { min: number, max: number }",
     },
   },
@@ -74,41 +74,12 @@ const itinerarySchema = new mongoose.Schema<IItinerary>({
     type: [Number],
     required: true,
     validate: {
-      validator: validateRating,
+      validator: itineraryValidator.validateRating,
       message: "Invalid rating format, must be a number between 0 and 5",
     },
   },
   preference_tags: { type: [String], required: true },
 });
-
-// Validate location coordinates to be within the range of longitude -180 to 180 and latitude -90 to 90
-function validateLocations(locations: [{ longitude: number; latitude: number }]) {
-  return locations.every((location) => {
-    return (
-      location.longitude >= CONSTANTS.MIN_LONGITUDE &&
-      location.longitude <= CONSTANTS.MAX_LONGITUDE &&
-      location.latitude >= CONSTANTS.MIN_LATITUDE &&
-      location.latitude <= CONSTANTS.MAX_LATITUDE
-    );
-  });
-}
-
-// Validate price format to be a number or an object { min: number, max: number } and to be greater than or equal to 0
-function validateTour_price(tour_price: number | { min: number; max: number }) {
-  if (typeof tour_price === "number") {
-    return tour_price >= CONSTANTS.ZERO;
-  } else if (typeof tour_price === "object") {
-    return tour_price.min >= CONSTANTS.ZERO && tour_price.max > tour_price.min;
-  }
-  return false;
-}
-
-// Validate rating format to be a number between 0 and 5
-function validateRating(ratings: number[]) {
-  return ratings.every(
-    (rating) => rating >= CONSTANTS.MIN_RATING && rating <= CONSTANTS.MAX_RATING,
-  );
-}
 
 const Itinerary = mongoose.model<IItinerary>("Itinerary", itinerarySchema);
 export default Itinerary;
