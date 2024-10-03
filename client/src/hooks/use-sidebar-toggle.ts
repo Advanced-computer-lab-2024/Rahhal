681e0be69cd8d4218bea31e06be0fb6207ec268e
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-10-03 22:20:09 +0300

    feat: add react custom hook to store global state variable using zustand

---
+++ client/src/hooks/use-sidebar-toggle.ts
@@ -0,0 +1,19 @@
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+
+interface SideBarToggleState {
+    isOpen: boolean;
+    setIsOpen: () => void;
+}
+
+export const useSideBarToggle = create(
+    persist<SideBarToggleState>((set,get) => ({
+        isOpen: true,
+        setIsOpen: () => {set({isOpen: !get().isOpen})}
+    })
+    ,
+    {
+        name: 'sidebarOpen',
+        storage: createJSONStorage(() => localStorage)
+    })
+)
