e38b6893ad45ec2cd5b3da0d2015692194eb7feb
Ziad Abdelrahman <127897386+Ziad-Abdelrahman@users.noreply.github.com>
2024-10-04 19:47:59 +0300

    Homepage Search Bar (#23)
    * feat(client): add tailwind and shadcn
    
    * refactor(client): remove unnecessary vite data
    
    * feat(client): add searchbar component
    
    * json packages
    
    * shadcn components & some refactoring
    
    * feat(client): add extra searchbar features
    
    * feat(client): added hot reload for windows
    
    * feat(client): add a  combobox to search for categories and tags

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,61 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import EntertainmentCard from "@/components/entertainment-card/EntertainmentCard";
-import Pyramid from "./assets/pyramids.webp";
-import felluca from "./assets/aswan2.webp";
-import ski from "./assets/ski egypt.jpg";
-import NonTouristView from "@/components/non-tourist/NonTouristView";
+import SearchBar from './components/shadcn/SearchBar.tsx'
 
-import "./App.css";
-
-function TouristAttractions() {
+export default function App() {
   return (
-    <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
-      <div>
-        <EntertainmentCard
-          image={Pyramid}
-          rating={5}
-          title={"Great Pyramids Of Giza"}
-          location="Giza"
-          price={200}
-          openingTime="Sunday-Saturday 7AM-5PM"
-        />
+    <>
+    <div className="flex items-center justify-center h-screen">
+      <SearchBar/>
       </div>
-
-      <div>
-        <EntertainmentCard
-          image={ski}
-          rating={4.8}
-          title={"Ski Egypt"}
-          location={"6th Of October"}
-          price={200}
-          availability={true}
-        />
-      </div>
-      <div>
-        <EntertainmentCard
-          image={felluca}
-          rating={4.3}
-          title={"Felluca Ride in Nile Aswan"}
-          location="Aswan"
-          price={300}
-          language={["English", "Arabic", "Italian"]}
-        />
-      </div>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<TouristAttractions />} />
-          <Route path="/non-tourist" element={<NonTouristView />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App;
