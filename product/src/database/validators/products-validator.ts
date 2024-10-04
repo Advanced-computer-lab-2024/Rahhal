248f0f841cfecc581276ffecfaac2045ada75fad
ahdmostafa0 <ahdmostafa327@gmail.com>
2024-10-04 17:05:42 +0300

    refactor: - Change products schema from accpeting price range - Add swagger

--- product/src/database/validators/products-validator.ts
+++ product/src/database/validators/products-validator.ts
@@ -1,17 +1,14 @@
 import { CONSTANTS } from "../../utils/constants";
 
-
-// Validate price format to be a number or an object { min: number, max: number } and to be greater than or equal to 0
-export function validatePrice(price: number | { min: number; max: number }) {
-    if (typeof price === "number") {
-        return price >= CONSTANTS.ZERO;
-    } else if (typeof price === "object") {
-        return price.min >= CONSTANTS.ZERO && price.max > price.min;
-    }
-    return false;
+// Validate price format to be a number and greater than 0
+export function validatePrice(price: number) {
+  if (typeof price === "number") {
+    return price >= CONSTANTS.ZERO;
+  }
+  return false;
 }
 
 // Validate rating format to be a number between 0 and 5
 export function validateRating(ratings: number) {
-    return ratings >= CONSTANTS.MIN_RATING && ratings <= CONSTANTS.MAX_RATING;
+  return ratings >= CONSTANTS.MIN_RATING && ratings <= CONSTANTS.MAX_RATING;
 }
