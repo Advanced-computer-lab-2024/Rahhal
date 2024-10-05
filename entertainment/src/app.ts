4013071693c1a4bca2f15ff62f4f59f23ed31e02
Talaat-jr <abdelrahman.mohamed@instabug.com>
2024-10-05 18:35:06 +0300

    refactor-entertainment: Update itineraries service endpoints and fix typo in route

--- entertainment/src/app.ts
+++ entertainment/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import activitiesRoutes from "./api/routes/activities-routes";
-import ItinerariesRoutes from "./api/routes/itineraries-routes";
+import itinerariesRoutes from "./api/routes/itineraries-routes";
 import preferenceTagRoutes from "./api/routes/preference-tags-routes";
 import categoryRoutes from './api/routes/category-routes';
 import swaggerUi from "swagger-ui-express";
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Routes
 app.use("/activities", activitiesRoutes);
-app.use("/itenaries", ItinerariesRoutes);
+app.use("/itineraries", itinerariesRoutes);
 app.use('/categories',categoryRoutes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/preference-tags", preferenceTagRoutes);
