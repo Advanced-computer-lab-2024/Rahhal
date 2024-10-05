a4d844e6dbf6b6f450adc89c5fca95b395469975
Dodoz <125830499+AhdMostafa0@users.noreply.github.com>
2024-10-06 01:58:13 +0300

    product-feat/Finish products CRUD endpoints (#22)
    * feat: finish CRUDs for products
    
    * refactor: - Change products schema from accpeting price range
    - Add swagger
    
    * refactor: - Replace PUT with PATCH for product updates
    - Remove unnecessary promises
    - Improve error handling
    - Replace magic numbers with named constants
    
    * refactor-products: Change product_name to name in the products model
    
    * refactor: change catch clause structure
    
    * refactor: change rating from number to a rating object
    
    * fix: change rating from number to an object
    
    ---------
    
    Co-authored-by: Farahh23 <125885412+Farahh23@users.noreply.github.com>

---
+++ product/src/server.ts
@@ -0,0 +1,22 @@
+import app from "./app";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+async function connectToDB() {
+    try {
+        await mongoose.connect(MONGODB_URI as string);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB: ", error);
+    }
+}
+
+await connectToDB();
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
