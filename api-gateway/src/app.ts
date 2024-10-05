702c4580b51747351392f4b29a174f63cb0a1255
Talaat-jr <abdelrahman.mohamed@instabug.com>
2024-10-05 03:14:44 +0300

    feat(api-gateway): Add product routes and service calls
    - Added product-routes.ts file to handle routes related to products
    - Implemented getAllProducts, getProductById, createProduct, updateProduct, and deleteProduct functions in product-controllers.ts
    - Created product-service.ts to handle service calls for products
    - Updated app.ts to include the new product routes

--- api-gateway/src/app.ts
+++ api-gateway/src/app.ts
@@ -1,15 +1,18 @@
 import express from "express";
-import morgan from "morgan";
-import entertainmentRouter from "@/api/routes/enterainment-routes";
-import userRouter from "@/api/routes/user-routes"
+import morgan from 'morgan';
+import entertainmentRoutes from "@/api/routes/enterainment-routes";
+import userRoutes from "@/api/routes/user-routes";
+import productRoutes from "@/api/routes/product-routes";
 
 const app = express();
 
+// Middleware
 app.use(express.json());
-
 app.use(morgan("dev"));
 
-app.use("/entertainment", entertainmentRouter);
-app.use("/users", userRouter);
+// Routes
+app.use("/api/entertainment", entertainmentRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/product", productRoutes);
 
 export default app;
