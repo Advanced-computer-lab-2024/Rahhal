8d097aab1d7cf3d31f8d9bcfb8db7ae6190c8588
Amr Hegazy <68474553+Amr-Hegazy1@users.noreply.github.com>
2024-10-06 02:14:14 +0300

    feat: Added generic modal (#19)
    * feat: Added generic modal
    
    * refactor: Update `GenericModal` component styles to be max-height instead of a fixed height
    
    * refactor: Update `GenericModal` component to use conditional rendering for children
    
    * fix: Fix No overflow problem in modal
    
    * refactor: Update `GenericModal` component to include an onSubmit prop
    
    - The `GenericModal` component has been updated to include an `onSubmit` prop, which allows the parent component to handle the submission logic when the "Save changes" button is clicked.

---
+++ client/src/components/GenericModal.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import React from "react";
+
+interface GenericModalProps {
+  title: string;
+  description: string;
+  dialogTrigger: React.ReactNode;
+  children: React.ReactNode[];
+  onSubmit: () => void;
+}
+
+export function GenericModal({ title, description, dialogTrigger, children, onSubmit }: GenericModalProps) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{dialogTrigger}</DialogTrigger>
+      <DialogContent className="width-full max-h-[80%] rounded-md border p-4 overflow-auto">
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div className="flex-1 py-4 overflow-auto">
+          {children && children.map((child) => <div className="grid items-center">{child}</div>)}
+        </div>
+        <DialogFooter>
+          <Button onSubmit={onSubmit}>Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
