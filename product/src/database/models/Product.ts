3de5aa07ef44b732ae977e8a063bce831498112d
Dodoz <125830499+AhdMostafa0@users.noreply.github.com>
2024-10-06 01:58:13 +0300

    product-feat/Finish products CRUD endpoints (#22)
    * feat: finish CRUDs for products
    
    * refactor: - Change products schema from accpeting price range
    - Add swagger
    
    * refactor: - Replace PUT with PATCH for product updates
    - Remove unnecessary promises
    - Improve error handling
    - Replace magic numbers with named constants
    
    * refactor-products: Change product_name to name in the products model
    
    * refactor: change catch clause structure
    
    * refactor: change rating from number to a rating object
    
    * fix: change rating from number to an object
    
    ---------
    
    Co-authored-by: Farahh23 <125885412+Farahh23@users.noreply.github.com>

---
+++ product/src/database/models/Product.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import * as productValidator from "../validators/products-validator";
+import type { IRating } from "../rating";
+import { ratingSchema, validateRatings } from "../rating";
+export interface IProduct {
+  name: string;
+  picture: string;
+  price: number;
+  description: string;
+  seller: string;
+  ratings: IRating[];
+  reviews: { user: string; rating: number; comment: string }[];
+}
+
+const productSchema = new mongoose.Schema<IProduct>({
+  name: { type: String, required: true },
+  picture: { type: String, required: true },
+  price: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: productValidator.validatePrice,
+      message: "Invalid price format, must be a number greater than 0",
+    },
+  },
+  description: { type: String, required: true },
+  seller: { type: String, required: true },
+  ratings: {
+    type: [ratingSchema],
+    validate: {
+      validator: validateRatings,
+      message: "Invalid rating format, must be a number between 0 and 5",
+    },
+  },
+});
+
+const Product = mongoose.model<IProduct>("Product", productSchema);
+export default Product;
