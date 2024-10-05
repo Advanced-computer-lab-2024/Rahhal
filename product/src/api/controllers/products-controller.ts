ff82256466600d3a4cf8064b59a4ffa4c66a2164
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-10-06 01:03:25 +0300

    refactor: change catch clause structure

--- product/src/api/controllers/products-controller.ts
+++ product/src/api/controllers/products-controller.ts
@@ -3,84 +3,68 @@ import * as productsService from "../../services/products-service";
 import { STATUS_CODES } from "../../utils/constants";
 
 export async function getAllProducts(req: express.Request, res: express.Response) {
-    try {
-        const products = await productsService.getAllProducts();
-        res.status(STATUS_CODES.STATUS_OK).json(products);
-    } catch (error) {
-        if (error instanceof Error) {
-            res.status(STATUS_CODES.SERVER_ERROR).json({ message: error.message });
-        } else {
-            res.status(STATUS_CODES.SERVER_ERROR).json({ message: "An unknown error occurred" });
-        }
-    }
+  try {
+    const products = await productsService.getAllProducts();
+    res.status(STATUS_CODES.STATUS_OK).json(products);
+  } catch (error) {
+    res
+      .status(STATUS_CODES.SERVER_ERROR)
+      .json({ message: error instanceof Error ? error.message : "An unknown error occurred" });
+  }
 }
 
 export async function getProductById(req: express.Request, res: express.Response) {
-    try {
-        const product = await productsService.getProductById(req.params.id);
-        if (!product) {
-            res.status(STATUS_CODES.NOT_FOUND).json({ message: "Product not found" });
-        } else {
-            res.status(STATUS_CODES.STATUS_OK).json(product);
-        }
+  try {
+    const product = await productsService.getProductById(req.params.id);
+    if (!product) {
+      res.status(STATUS_CODES.NOT_FOUND).json({ message: "Product not found" });
+    } else {
+      res.status(STATUS_CODES.STATUS_OK).json(product);
     }
-    catch (error) {
-        if (error instanceof Error) {
-            res.status(STATUS_CODES.SERVER_ERROR).json({ message: error.message });
-        } else {
-            res.status(STATUS_CODES.SERVER_ERROR).json({ message: "An unknown error occurred" });
-        }
-    }
+  } catch (error) {
+    res
+      .status(STATUS_CODES.SERVER_ERROR)
+      .json({ message: error instanceof Error ? error.message : "An unknown error occurred" });
+  }
 }
 
 export async function createProduct(req: express.Request, res: express.Response) {
-    try {
-        const product = await productsService.createProduct(req.body);
-        res.status(STATUS_CODES.CREATED).json(product);
-    }
-    catch (error) {
-        if (error instanceof Error) {
-            res.status(STATUS_CODES.SERVER_ERROR).json({ message: error.message });
-        } else {
-            res.status(STATUS_CODES.SERVER_ERROR).json({ message: "An unknown error occurred" });
-        }
-    }
+  try {
+    const product = await productsService.createProduct(req.body);
+    res.status(STATUS_CODES.CREATED).json(product);
+  } catch (error) {
+    res
+      .status(STATUS_CODES.SERVER_ERROR)
+      .json({ message: error instanceof Error ? error.message : "An unknown error occurred" });
+  }
 }
 
 export async function updateProduct(req: express.Request, res: express.Response) {
-    try {
-        const product = await productsService.updateProduct(req.params.id, req.body);
-        if (!product) {
-            res.status(STATUS_CODES.NOT_FOUND).json({ message: "Product not found" });
-        } else {
-            res.status(STATUS_CODES.STATUS_OK).json(product);
-        }
-    } catch (error) {
-        if (error instanceof Error) {
-            res.status(STATUS_CODES.SERVER_ERROR).json({ message: error.message });
-        } else {
-            res.status(STATUS_CODES.SERVER_ERROR).json({ message: "An unknown error occurred" });
-        }
+  try {
+    const product = await productsService.updateProduct(req.params.id, req.body);
+    if (!product) {
+      res.status(STATUS_CODES.NOT_FOUND).json({ message: "Product not found" });
+    } else {
+      res.status(STATUS_CODES.STATUS_OK).json(product);
     }
+  } catch (error) {
+    res
+      .status(STATUS_CODES.SERVER_ERROR)
+      .json({ message: error instanceof Error ? error.message : "An unknown error occurred" });
+  }
 }
 
 export async function deleteProduct(req: express.Request, res: express.Response) {
-    try {
-        const product = await productsService.deleteProduct(req.params.id);
-        if (!product) {
-            res.status(STATUS_CODES.NOT_FOUND).json({ message: "Product not found" });
-        } else {
-            res.status(STATUS_CODES.STATUS_OK).json(product);
-        }
-    } catch (error) {
-        if (error instanceof Error) {
-            res.status(STATUS_CODES.SERVER_ERROR).json({ message: error.message });
-        } else {
-            res.status(STATUS_CODES.SERVER_ERROR).json({ message: "An unknown error occurred" });
-        }
+  try {
+    const product = await productsService.deleteProduct(req.params.id);
+    if (!product) {
+      res.status(STATUS_CODES.NOT_FOUND).json({ message: "Product not found" });
+    } else {
+      res.status(STATUS_CODES.STATUS_OK).json(product);
     }
+  } catch (error) {
+    res
+      .status(STATUS_CODES.SERVER_ERROR)
+      .json({ message: error instanceof Error ? error.message : "An unknown error occurred" });
+  }
 }
-
-
-
-
