6fbfa71cea2d34796012755c9a9b1d7ba55f2736
Ahmed Hawater <63217442+AhmedHawater2003@users.noreply.github.com>
2024-10-06 04:31:45 +0300

    api-gateway-feat: Create google maps services endpoints (#36)
    * feat: Add place id to the location field in the db
    
    * feat: Add endpoint to get place details using place id
    
    * refactor: rename place full name field to description
    
    * feat: Add endpoint to get place details from long and lat
    
    * refactor: Move constants to shared util file
    
    ---------
    
    Co-authored-by: root <root@HAWATER-LAPTOP>

--- api-gateway/src/services/google-maps-service.ts
+++ api-gateway/src/services/google-maps-service.ts
@@ -1,4 +1,5 @@
-import axios from 'axios'
+import { CONSTANTS } from "@/utils/constants";
+import axios from "axios";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -12,13 +13,37 @@ interface IAutocompletePrediction {
 }
 
 export async function getPlaceAutocomplete(place: string) {
-  const { data } = await axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${place}&key=${GOOGLE_MAPS_API_KEY}`);
+  const { data } = await axios.get(
+    `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${place}&key=${GOOGLE_MAPS_API_KEY}`,
+  );
   const predictions = data.predictions;
   return predictions.map((prediction: IAutocompletePrediction) => {
     return {
-      "description": prediction.description,
-      "place_id": prediction.place_id,
-      "reference": prediction.reference
+      description: prediction.description,
+      place_id: prediction.place_id,
+      reference: prediction.reference,
     };
   });
+}
+
+export async function getPlaceDetailsById(placeId: string) {
+  const { data } = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?place_id=${placeId}&key=${GOOGLE_MAPS_API_KEY}`,
+  );
+  const { formatted_address, geometry } = data.results[CONSTANTS.ZERO];
+  return {
+    description: formatted_address,
+    location: geometry.location,
+  };
+}
+
+export async function getPlaceDetailsByLocation(location: { lat: number; lng: number }) {
+  const { data } = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.lat},${location.lng}&key=${GOOGLE_MAPS_API_KEY}&location_type=APPROXIMATE`,
+  );
+  const { formatted_address, place_id } = data.results[CONSTANTS.ZERO];
+  return {
+    description: formatted_address,
+    placeId: place_id,
+  };
 }
