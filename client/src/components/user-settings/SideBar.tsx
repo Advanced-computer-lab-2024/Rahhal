40958befe27223b85cb2b264c178d187061ec86d
YousefElbrolosy <yousefelbrolosy@gmail.com>
2024-10-06 15:21:58 +0300

    feat: made navigating with single page architecture behavior

--- client/src/components/user-settings/SideBar.tsx
+++ client/src/components/user-settings/SideBar.tsx
@@ -1,34 +1,36 @@
 import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function SideBar() {
+  const location = useLocation();
   const [currentPath, setCurrentPath] = useState("");
 
   useEffect(() => {
-    setCurrentPath(window.location.pathname);
-  }, []);
+    setCurrentPath(location.pathname);
+  }, [location.pathname]);
 
   const sideBarElements = [
-    { title: "Profile", href: "/user-settings" },
-    { title: "Account", href: "/user-settings/account" },
+    { title: "Profile", to: "/user-settings" },
+    { title: "Account", to: "/user-settings/account" },
   ];
 
   return (
     <aside className="-mx-4 lg:w-1/5" style={{ minWidth: "100%" }}>
       <nav className="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1">
         {sideBarElements.map((element, index) => {
-          // Check if the current path matches the element's href
-          const isActive = currentPath === element.href;
+          // Check if the current path matches the element's to
+          const isActive = currentPath === element.to;
 
           return (
-            <a
+            <Link
               key={index}
-              href={element.href}
+              to={element.to}
               className={`inline-flex items-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 h-9 px-4 py-2 justify-start
                 ${isActive ? "bg-muted text-accent-foreground" : "hover:bg-transparent hover:underline"}
               `}
             >
               {element.title}
-            </a>
+            </Link>
           );
         })}
       </nav>
