a580174633b3148b70fce295f5fd063e47b61ab2
Ziad Abdelrahman <127897386+Ziad-Abdelrahman@users.noreply.github.com>
2024-10-07 23:25:14 +0300

    User fix/email duplicate error (#64)
    * refactor: Refactor Generic modal on submit
    
    * refactor: Refactor Generic modal on submit
    
    * fix: Not able to create an admin due to duplicates email
    
    ---------
    
    Co-authored-by: Amr Hegazy <68474553+Amr-Hegazy1@users.noreply.github.com>

--- user/src/database/models/User.ts
+++ user/src/database/models/User.ts
@@ -1,7 +1,7 @@
 import type { Types } from "mongoose";
 import mongoose, { Schema } from "mongoose";
 import userValidators from "../../validators/user-validators";
-enum Role {
+export enum Role {
   admin = "admin",
   tourist = "tourist",
   tourGuide = "tourGuide",
@@ -31,7 +31,7 @@ export interface IUser {
   companyProfile?: string;
   companyName?: string;
   description?: string;
-  wallet?:number;
+  wallet?: number;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -45,7 +45,7 @@ export interface IUser {
 // interface IWallet{
 //   balance:number;
 //   creditCard:ICreditCard;
-  
+
 // }
 
 const userSchema: Schema = new Schema<IUser>(
@@ -86,14 +86,16 @@ const userSchema: Schema = new Schema<IUser>(
     },
     email: {
       type: String,
-      unique: true,
+      // unique: function () {
+      //   return this.role !== Role.admin && this.role !== Role.tourismGovernor;
+      // },
       required: function () {
         return this.role !== Role.tourismGovernor && this.role !== Role.admin;
       },
-      validate: {
-        validator: userValidators.validateEmail,
-        message: "Invalid email entry",
-      },
+      // validate: {
+      //   validator: userValidators.validateEmail,
+      //   message: "Invalid email entry",
+      // },
     },
     password: {
       type: String,
@@ -234,9 +236,9 @@ const userSchema: Schema = new Schema<IUser>(
         message: "Invalid description entry",
       },
     },
-    wallet:{
-      type:Number,
-      default:0,
+    wallet: {
+      type: Number,
+      default: 0,
       validate: {
         validator: userValidators.validateWallet,
         message: "Invalid wallet entry",
