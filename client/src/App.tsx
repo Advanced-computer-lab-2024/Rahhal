73192b4b46d58f54723fcbb0b9900f23b8f3da44
Amr Hegazy <68474553+Amr-Hegazy1@users.noreply.github.com>
2024-10-07 17:47:15 +0300

    feat: Add Admin Homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import DetailsPage from "./components/home-page/DetailsPage.tsx"
 import TouristHomePage from "./TouristHomePage/TouristHomePage.tsx"
 import SignupSelector from "./components/forms/SignupSelector.tsx"
+import AdminHomepage from "./components/non-tourist/admin/AdminHomepage.tsx"
+import CategoryView from "./components/non-tourist/admin/CategoriesTable.tsx"
 
 export default function App() {
   const queryClient = new QueryClient ();
@@ -18,7 +20,12 @@ export default function App() {
           <Route path="/signup" element={<SignupSelector />}/>
           <Route path="/signin" element={<></>} />
           <Route path="/details/:id" element={<DetailsPage />} />
+          <Route path="/admin" element={<AdminHomepage />} />
+          <Route path="/admin/categories" element={<CategoryView />}/>
         </Routes>
+        
+          
+        
       </Router>
     </QueryClientProvider>
     </>
