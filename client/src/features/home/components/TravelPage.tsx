346b5af1a8f0017aa66fbe638a890f542d3ed689
Farah Faisal <125885412+FarahFaisall@users.noreply.github.com>
2024-11-04 01:30:20 +0200

    Client feat/travel search bar (#90)
    * feat: add counter that lets user selet number of passengers
    
    * client-refactor: add small changes to home page to make it adapt with new search and its props
    
    * feat: add date-time pciker to pick dates in side bar
    
    * refactor: add some props to adapt with new search bar props
    
    * feat: add more components to search bar to make it generic
    
    * feat: add search bar route
    
    * feat: add on input change in unoutBox (optional)
    
    ---------
    
    Co-authored-by: Farahh23 <125885412+Farahh23@users.noreply.github.com>

---
+++ client/src/features/home/components/TravelPage.tsx
@@ -0,0 +1,12 @@
+import TravelSearchBar from "./TravelSearchBar";
+function TravelPage() {
+  return (
+    <>
+      <div className="flex justify-center items-center w-[100%] h-[66px]">
+        <TravelSearchBar />
+      </div>
+    </>
+  );
+}
+
+export default TravelPage;
