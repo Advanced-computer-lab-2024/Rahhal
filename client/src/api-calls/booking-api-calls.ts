f9f2f06d108998b62b0fbb1cab2f303e527a7093
Abdallah Ahmed <101679255+AbdallahAhmadd@users.noreply.github.com>
2024-11-04 03:43:44 +0200

    create booking api call for tourist in frontend (#92)
    * feat(client): create api calls for bookings
    
    * feat(client): add booking type and interface
    
    * feat(client): add booking service url
    
    * refactor(client): change signatures to functions
    
    * chore: add @types/express-fileupload dependancy

---
+++ client/src/api-calls/booking-api-calls.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { SERVICES_URLS } from "@/lib/constants";
+import type { TBookingType, IBooking } from "@/features/home/types/home-page-types";
+
+export async function fetchAllBookings(){
+    const response = await axios.get(SERVICES_URLS.BOOKING + `/bookings`);
+    return response.data;
+}
+
+export async function fetchUserBookings(userId: string) {
+    const response = await axios.get(SERVICES_URLS.BOOKING + `/bookings`, {
+        params: {
+            userId: userId,
+        },
+    });
+    return response.data;
+};
+
+export async function createBooking (bookingData: TBookingType){
+    const newBooking = {
+        user: bookingData.user,
+        entity: bookingData.entity,
+        type: bookingData.type,
+    }
+    const response = await axios.post(SERVICES_URLS.BOOKING + `/bookings`, newBooking);
+    return response.data;
+}
+
+export async function updateBookingRequest(id: string, bookingData: Partial<IBooking>){
+    const response = await axios.patch(
+        `${SERVICES_URLS.BOOKING}/bookings/${id}`,
+        bookingData
+    );
+    return response.data;
+};
+
+
+
+
+
+
