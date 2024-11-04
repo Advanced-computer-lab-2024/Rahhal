5f674bcfc3d317e29db7a55c38d91626eadced75
YousefElbrolosy <yousefelbrolosy@gmail.com>
2024-11-04 14:30:21 +0200

    feat: added code to detect which type of visa it is (Visa or MasterCard) and display image

--- client/src/features/user-settings/components/PaymentMethod.tsx
+++ client/src/features/user-settings/components/PaymentMethod.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { Icons } from "@/components/ui/icons"
+import { Icons } from "@/components/ui/icons";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -9,36 +9,76 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import visaLogo from "@/assets/visalogo.png";
+import mastercardLogo from "@/assets/Mastercard-logo.png";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
+import { useState } from "react";
 
 export function CardsPaymentMethod() {
+  const [cardType, setCardType] = useState<string | null>(null);
+  function GetCardType(number: string) {
+    // visa
+    var re = new RegExp("^4");
+    if (number.match(re) != null) return "Visa";
+
+    // Mastercard
+    // Updated for Mastercard 2017 BINs expansion
+    if (
+      /^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$/.test(
+        number,
+      )
+    )
+      return "Mastercard";
+
+    // AMEX
+    re = new RegExp("^3[47]");
+    if (number.match(re) != null) return "AMEX";
+
+    // Discover
+    re = new RegExp(
+      "^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)",
+    );
+    if (number.match(re) != null) return "Discover";
+
+    // Diners
+    re = new RegExp("^36");
+    if (number.match(re) != null) return "Diners";
+
+    // Diners - Carte Blanche
+    re = new RegExp("^30[0-5]");
+    if (number.match(re) != null) return "Diners - Carte Blanche";
+
+    // JCB
+    re = new RegExp("^35(2[89]|[3-8][0-9])");
+    if (number.match(re) != null) return "JCB";
+
+    // Visa Electron
+    re = new RegExp("^(4026|417500|4508|4844|491(3|7))");
+    if (number.match(re) != null) return "Visa Electron";
+
+    return "";
+  }
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>Payment Method</CardTitle>
-        <CardDescription>
-          Add a new payment method to your account.
-        </CardDescription>
+        <CardDescription>Add a new payment method to your account.</CardDescription>
       </CardHeader>
       <CardContent className="grid gap-6">
         <RadioGroup defaultValue="card" className="grid grid-cols-3 gap-4">
           <div>
-            <RadioGroupItem
-              value="card"
-              id="card"
-              className="peer sr-only"
-              aria-label="Card"
-            />
+            <RadioGroupItem value="card" id="card" className="peer sr-only" aria-label="Card" />
             <Label
               htmlFor="card"
               className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-transparent p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
@@ -77,12 +117,7 @@ export function CardsPaymentMethod() {
           </div>
 
           <div>
-            <RadioGroupItem
-              value="apple"
-              id="apple"
-              className="peer sr-only"
-              aria-label="Apple"
-            />
+            <RadioGroupItem value="apple" id="apple" className="peer sr-only" aria-label="Apple" />
             <Label
               htmlFor="apple"
               className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-transparent p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
@@ -102,7 +137,29 @@ export function CardsPaymentMethod() {
         </div>
         <div className="grid gap-2">
           <Label htmlFor="number">Card number</Label>
-          <Input id="number" placeholder="" />
+          <div className="flex w-full items-center space-x-2">
+            <Input
+              id="number"
+              placeholder=""
+              onChange={(e) => {
+                const cardType = GetCardType(e.target.value);
+                setCardType(cardType);
+                console.log(cardType);
+              }}
+            />
+            <div className="absolute right-12 flex items-center h-full pointer-events-none">
+              {cardType === "Visa" && (
+                <img src={visaLogo} alt="Visa Logo" className="h-11 mr-4 w-auto object-contain" />
+              )}
+              {cardType === "Mastercard" && (
+                <img
+                  src={mastercardLogo}
+                  alt="Mastercard Logo"
+                  className="h-8 mr-4 w-auto object-contain"
+                />
+              )}
+            </div>
+          </div>
         </div>
         <div className="grid grid-cols-3 gap-4">
           <div className="grid gap-2">
@@ -152,5 +209,5 @@ export function CardsPaymentMethod() {
         <Button className="w-full">Continue</Button>
       </CardFooter>
     </Card>
-  )
+  );
 }
