f11d38ea074a402f262736f81bf7d8a632f8f991
Abdallah Ahmed <101679255+AbdallahAhmadd@users.noreply.github.com>
2024-11-04 17:19:13 +0200

    Overview card in all details pages(Activities and Itinerary) (#91)

---
+++ client/src/components/shadcn/ComboboxDemo.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import * as React from "react"
+import { Check, ChevronsUpDown } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+// Define the prop types for the component
+interface Option {
+  value: string
+  label: string
+}
+
+interface ComboboxDemoProps {
+  data?: Option[]
+  onSelect?: (selectedValue: string) => void
+}
+
+export function ComboboxDemo({ data = [], onSelect }: ComboboxDemoProps) { // Default data to an empty array
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState("")
+
+  const handleSelect = (selectedValue: string) => {
+    setValue(selectedValue)
+    setOpen(false)
+    onSelect?.(selectedValue) 
+  }
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? data.find((item) => item.value === value)?.label
+            : "Select your desired date..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandEmpty>No options found.</CommandEmpty>
+            <CommandGroup>
+              {data.map((item) => (
+                <CommandItem
+                  key={item.value}
+                  value={item.value}
+                  onSelect={handleSelect} 
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === item.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {item.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
