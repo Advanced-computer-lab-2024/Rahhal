12893ed5598aaa8215833bb43742d398470c5d79
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-11-04 21:33:21 +0200

    feat(client): add props to card component

--- client/src/features/home/components/MyTripsCard.tsx
+++ client/src/features/home/components/MyTripsCard.tsx
@@ -3,42 +3,49 @@ import felluca from "@/assets/aswan2.webp";
 import { IoMdMore } from "react-icons/io";
 
 interface MyTripsCardProps {
-    image?: string;
-    title?: string;
-    price?:number;
-    date?: string;
-    status?: string;
-    onClick?: () => void;
+  image?: string;
+  title?: string;
+  price?: number;
+  date?: string;
+  status?: string;
+  onClick?: () => void;
 }
 
-export const MyTripsCard: React.FC<MyTripsCardProps> = ({
-image,
-title,
-price,
-date,
-status,
-onClick,
-}) => {
+
+
+export const MyTripsCard: React.FC<MyTripsCardProps> = ({ image, title, price, date, status,onClick }) => {
   return (
-    <div className={MyTripStyles["card-container"]}>
+    <div className={MyTripStyles["card-container"]} onClick={onClick}>
       <div className={MyTripStyles["trip-details"]}>
         <div className={MyTripStyles["trip-details_image-container"]}>
           <img
-            src={image?image : felluca}
+            src={image ? image : felluca}
             className={MyTripStyles["trip-details_image-container_image"]}
             alt="felluca"
           />
         </div>
 
         <div className={MyTripStyles["trip-details_data"]}>
-          <p>{title? title: "Felluca Ride"}</p>
-          <p>{date? date : "28th July 11:00AM"}</p>
-          <p>{status?status:"Upcoming"}</p>
+          <p>{title}</p>
+          <p>{date}</p>
+          <p
+             className={
+              status === "upcoming"
+                ? MyTripStyles.statusUpcoming
+                : status === "cancelled"
+                ? MyTripStyles.statusCanceled
+                : status === "completed"
+                ? MyTripStyles.statusCompleted
+                : ""
+            }
+          >
+            {status}
+          </p>
         </div>
       </div>
       <div className={MyTripStyles["trip-price-container"]}>
         <div className={MyTripStyles["trip-price-container_text-icon"]}>
-          <p>{price?price:"EGP500"}</p>
+          <p>EGP {price}</p>
           <IoMdMore />
         </div>
       </div>
