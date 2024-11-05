bfe144d9f1445dab0611071feb56d130a86f0538
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-11-05 23:03:13 +0200

    feat(client): add my trips navigation in avatar

--- client/src/features/home/components/ProfileAvatar.tsx
+++ client/src/features/home/components/ProfileAvatar.tsx
@@ -46,9 +46,8 @@ export const ProfileAvatar = () => {
         <div className={AvatarStyles["menu"]}>
           <Link to={`/user-settings/${id}`} onClick={toggleDropdown}> <div className={AvatarStyles["menuItem"]}>Account</div></Link>
           <Link to={`/my-trips/${id}`} onClick={toggleDropdown}> <div className={AvatarStyles["menuItem"]}>Trips</div></Link>
-          <Link to={`/user-settings/wallet/${id}`}>
-            <div className={AvatarStyles["menuItem"]}>Wallet</div>
-          </Link>
+          <Link to={`/my-orders/${id}`}> <div className={AvatarStyles["menuItem"]} onClick={toggleDropdown}>My Orders</div></Link>
+          <div className={AvatarStyles["menuItem"]} onClick={toggleDropdown}>Wallet</div>
           <div className={AvatarStyles["menuItem"]} onClick={toggleDropdown}>Loyalty</div>
           <Link to={`/help-center/${id}`} onClick={toggleDropdown}>
             <div className={AvatarStyles["menuItem"]}>Help Center</div>
