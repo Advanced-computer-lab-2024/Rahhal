62146d83c6f36f9b2f03a963ff72a09eff9a114b
Ali Shokry <125921562+AliShokryy@users.noreply.github.com>
2024-11-05 21:13:34 +0200

    User schema update to handle loyality points and balance  (#106)
    * feat: updated user schema ato contain points and user prefereneces
    
    * feat: add redeem loyalty points as balance in wallet , receive points on booking and retrieve balance on cancellation
    
    * refactor: removed updateUserByUsername from user-controller in user endpoint

--- user/src/utils/constants.ts
+++ user/src/utils/constants.ts
@@ -1,9 +1,19 @@
 export const STATUS_CODES = {
   NOT_FOUND: 404,
-  CONFLICT: 409, 
+  CONFLICT: 409,
   BAD_REQUEST: 400,
   CREATED: 201,
   STATUS_OK: 200,
   SERVER_ERROR: 500,
   UNPROCESSABLE_ENTITY: 422,
+};
+
+export const points = {
+  level1maxPoints: 100000,
+  level1PointRate: 0.5,
+  level2maxPoints: 500000,
+  level2PointRate: 1,
+  level3PointRate: 1.5,
+  minPoints: 10000,
+  amountForMinPoints: 100,
 };
