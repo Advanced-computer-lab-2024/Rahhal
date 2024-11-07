d1945245e188169a140a3731d291154c49fbb022
Amr Hegazy <68474553+Amr-Hegazy1@users.noreply.github.com>
2024-11-07 23:34:58 +0200

    firebase upload request refactor (#130)
    * refactor: upload to firebase sends multiple files all at once and not one at a time
    
    refactor: made upload smoother
    
    fix: fix `createActivity` not uploading images
    
    * refactor: optimize uploadToFirebaseReady to handle file uploads more efficiently

--- client/src/utils/firebase.ts
+++ client/src/utils/firebase.ts
@@ -7,28 +7,34 @@ export async function uploadToFirebase(
   id: string | undefined,
   fileRenameCallback: (files: FileList, owner: string, id: string | undefined) => FileList,
 ): Promise<string[]> {
-  const urls: string[] = [];
-  if (!files) return urls;
+  
+  if (!files) return [];
   files = fileRenameCallback(files, owner, id ?? "");
+  const formData = new FormData();
   for (let i = 0; i < files!.length; i++) {
-    const formData = new FormData();
+    
     formData.append("image" + i, files![i]);
-    const response = await axios.post(SERVICES_URLS.FIREBASE + "/upload-multiple-files", formData);
-    urls.push((response.data as string[])[0]);
+   
+    
   }
+  const response = await axios.post(SERVICES_URLS.FIREBASE + "/upload-multiple-files", formData);
+  const urls: string[] = response.data as string[];
   return urls;
 }
 
 export async function uploadToFirebaseReady(
   files: File [] | null,
 ): Promise<string[]> {
-  const urls: string[] = [];
-  if (!files) return urls;
+  
+  if (!files) return [];
+  const formData = new FormData();
   for (let i = 0; i < files!.length; i++) {
-    const formData = new FormData();
+   
     formData.append("image" + i, files![i]);
-    const response = await axios.post(SERVICES_URLS.FIREBASE + "/upload-multiple-files", formData);
-    urls.push((response.data as string[])[0]);
+    
   }
+
+  const response = await axios.post(SERVICES_URLS.FIREBASE + "/upload-multiple-files", formData);
+  const urls: string[] = response.data as string[];
   return urls;
 }
