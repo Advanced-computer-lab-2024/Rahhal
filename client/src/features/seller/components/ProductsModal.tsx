1f0144ca043066b98992abea586f42861c0ce6c7
Amr Hegazy <68474553+Amr-Hegazy1@users.noreply.github.com>
2024-11-08 00:58:29 +0200

    fix: add link to view product statistics in ProductsModal (#136)

--- client/src/features/seller/components/ProductsModal.tsx
+++ client/src/features/seller/components/ProductsModal.tsx
@@ -128,6 +128,7 @@ export function ProductModal({ productData, dialogTrigger, userId, username }: P
         handleFileUploadCallback={(files) => setProductImages(files)}
       />
       {!isNewProduct && <ReviewDisplay reviews={modalProductData?.ratings ?? sampleReviews} />}
+      {!isNewProduct && <a className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" href={"/seller/product/" + productData?._id}>View Product Stats</a>}
     </GenericModal>
   );
 }
