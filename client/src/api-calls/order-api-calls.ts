d3e3bd4dda1fefd0d79ec496b30a82dff74e29d4
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-11-07 22:07:05 +0200

    client(feat): add api call to add rating in item in order

--- client/src/api-calls/order-api-calls.ts
+++ client/src/api-calls/order-api-calls.ts
@@ -1,4 +1,5 @@
 import { SERVICES_URLS } from "@/lib/constants";
+import { TOrder } from "@/features/home/types/home-page-types";
 import axios from "axios";
 
 // fetch all orders from orders endpoint
@@ -24,5 +25,23 @@ export async function fetchProductOrders(productId: string) {
       productId: productId,
     },
   });
+  return response.data;
+}
+
+
+export async function rateProduct(
+  productId: string,
+  orderId: string,
+  rating: number,
+  review: string
+) {
+  const response = await axios.patch(
+    `${SERVICES_URLS.ORDER}/orders/rate-product/${orderId}`,
+    {
+      productId,
+      rating,
+      review,
+    }
+  );
   return response.data;
 }
