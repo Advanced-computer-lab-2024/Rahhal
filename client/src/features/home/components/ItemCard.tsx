0ca124761408b8deaf4a07cfdc104bf1c3e649b6
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-11-07 02:57:26 +0200

    feat: create item card to hold items/products in order

--- client/src/features/home/components/ItemCard.tsx
+++ client/src/features/home/components/ItemCard.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import styles from '@/features/home/styles/ItemCard.module.css';
-
+import imagePlaceHolder from "@/assets/imageNotavail.png";
+import { getPriceValue } from "./GeneralGridView";
+import { useCurrencyStore } from "@/stores/currency-exchange-store";
+import currencyExchange from '@/utils/currency-exchange';
 interface ItemCardProps {
-  imageSrc: string;
+  image: string;
   title: string;
-  price: string;
+  price: number;
   onRate?: () => void;
 }
 
-const ItemCard: React.FC<ItemCardProps> = ({ imageSrc, title, price,onRate }) => (
-  <div className={styles.card}>
-    <img src={imageSrc} alt={title} className={styles.image} />
-    <div className={styles.details}>
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.price}>{price}</p>
-      <a className={styles.rate} onClick={onRate}>Rate</a>
+
+const ItemCard: React.FC<ItemCardProps> = ({ image, title, price, onRate }) => {
+  
+  const { currency } = useCurrencyStore();
+  const convertedPrice = currencyExchange("EGP", price);
+  const displayPrice = convertedPrice ? convertedPrice.toFixed(0) : "N/A";
+
+  return (
+    <div className={styles.card}>
+      <img src={image} alt={title} className={styles.image} onError={(e) => e.currentTarget.src = imagePlaceHolder} />
+      <div className={styles.details}>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.price}>{currency} {displayPrice}</p>
+        <a className={styles.rate} onClick={onRate}>Rate</a>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ItemCard;
