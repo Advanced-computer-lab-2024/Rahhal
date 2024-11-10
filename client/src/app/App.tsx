6bd812b106c6a96bab3ef1e728f87a5c1edb6aae
Ziad Abdelrahman <127897386+Ziad-Abdelrahman@users.noreply.github.com>
2024-11-10 20:43:15 +0200

    Fix/share historical places (#173)
    * feat: add get historical place by id api call
    
    * fix details page for both products and historical places
    
    * fix: added share button
    
    * chore: add avail query
    
    * fix:formatting
    
    * fix:fixed url

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -71,8 +71,8 @@ export default function App() {
             </Route>
             <Route path="/signup" element={<SignupSelector />} />
             <Route path="/signin" element={<Login />} />
-            <Route path="/details/:placeid/:id" element={<DetailsPage />} />
-            <Route path="/details/:placeid/" element={<DetailsPage />} />
+            <Route path="/:type/details/:placeid/:id" element={<DetailsPage />} />
+            <Route path="/:type/details/:placeid/" element={<DetailsPage />} />
             <Route element={<SettingsView />}>
               <Route path="/user-settings/:id" element={<ProfileForm />} />
               <Route path="/user-settings/account/:id" element={<AccountForm />} />
