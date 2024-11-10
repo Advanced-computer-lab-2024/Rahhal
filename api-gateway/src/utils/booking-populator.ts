8c333151385c94292e154d7a228d6ce6a30a57b5
Talaat-jr <abdelrahman.mohamed@instabug.com>
2024-11-10 20:22:25 +0200

    api-gateway-refactor: streamline booking population logic and improve error handling

--- api-gateway/src/utils/booking-populator.ts
+++ api-gateway/src/utils/booking-populator.ts
@@ -2,131 +2,84 @@ import { userAxiosInstance, entertainmentAxiosInstance } from "@/utils/axios-ins
 import { bookingStatus, bookingType } from "@/utils/types";
 import type { IBooking, IActivity, IUser, IItinerary, PopulatedBooking } from "@/utils/types";
 import type { AxiosResponse } from "axios";
-import { CONSTANTS } from "./constants";
+
+function SupportedType(type: string): boolean {
+  return is3rdPartyBooking(type) || type === bookingType.Itinerary || type === bookingType.Activity;
+}
+
+function is3rdPartyBooking(type: string): boolean {
+  return type === bookingType.Flight || type === bookingType.Hotel || type === bookingType.Transportation;
+}
+
+async function populateEntity(entity: string, type: string): Promise<IActivity | IItinerary | string> {
+  let populatedEntity: AxiosResponse<IActivity | IItinerary> | undefined;
+  if (type === bookingType.Activity)
+    populatedEntity = await entertainmentAxiosInstance.get<IActivity>(`/activities/${entity}`);
+  else if (type === bookingType.Itinerary)
+    populatedEntity = await entertainmentAxiosInstance.get<IItinerary>(`/itineraries/${entity}`);
+  else
+    return entity;
+
+  return populatedEntity.data;
+}
+
+export async function populateBooking(booking: IBooking): Promise<PopulatedBooking | null> {
+  return populateBookingHelper(booking, undefined, undefined);
+}
 
 export async function populateBookings(
   bookings: IBooking[],
   filter: Partial<IBooking>,
 ): Promise<PopulatedBooking[]> {
-  let user: AxiosResponse<IUser> | undefined;
-  let entity: AxiosResponse<IActivity | IItinerary> | undefined;
+  let populatedUser: IUser | undefined;
+  let populatedEntity: IActivity | IItinerary | string | undefined;
 
   // Fetch user if filter specifies a user
   if (filter.user) {
-    user = await userAxiosInstance.get<IUser>(`/users/${filter.user}`);
+    populatedUser = (await userAxiosInstance.get<IUser>(`/users/${filter.user}`)).data;
   }
 
   // Fetch entity if filter specifies an entity
-  if (filter.entity) {
-    const entityUrl =
-      filter.type === bookingType.Activity
-        ? `/activities/${filter.entity}`
-        : `/itineraries/${filter.entity}`;
-    entity = await entertainmentAxiosInstance.get<IActivity | IItinerary>(entityUrl);
-    
+  if (filter.entity && filter.type) {
+    populatedEntity = await populateEntity(filter.entity, filter.type);
   }
 
   const populatedBookings = await Promise.all(
     bookings.map(async (booking) => {
-      
-      if (booking.type === bookingType.Activity || booking.type === bookingType.Itinerary) {
-        
-        if (!filter.user) {
-          user = await userAxiosInstance.get<IUser>(`/users/${booking.user}`);
-        }
-        if (!filter.entity) {
-          const entityUrl =
-            booking.type === bookingType.Activity
-              ? `/activities/${booking.entity}`
-              : `/itineraries/${booking.entity}`;
-          
-          entity = await entertainmentAxiosInstance.get<IActivity | IItinerary>(entityUrl);
-          
-        }
-        
-        return {
-          _id: booking._id,
-          user: {
-            _id: user!.data._id,
-            username: user!.data.username,
-            email: user!.data.email,
-            firstName: user!.data.firstName,
-            lastName: user!.data.lastName,
-            phoneNumber: user!.data.phoneNumber,
-            dob: user!.data.dob,
-            nationality: user!.data.nationality,
-          },
-          entity: entity!.data,
-          type: booking.type,
-          status: booking.status,
-          selectedPrice: booking.selectedPrice,
-          selectedDate: booking.selectedDate,
-        };
-      }
-      return null;
+      return populateBookingHelper(booking, populatedUser, populatedEntity);
     }),
   );
-
-  
 
   // Filter out any null values from the final array
   return populatedBookings.filter((booking) => booking !== null) as PopulatedBooking[];
 }
 
-export async function populateBooking(booking: IBooking, filter: Partial<IBooking>): Promise<PopulatedBooking | null> {
-  let user: AxiosResponse<IUser> | undefined;
-  let entity: AxiosResponse<IActivity | IItinerary> | undefined;
+export async function populateBookingHelper(booking: IBooking, user: IUser | undefined, entity: IActivity | IItinerary | string | undefined): Promise<PopulatedBooking | null> {
+  let populatedUser: IUser | undefined;
+  let populatedEntity: IActivity | IItinerary | string | undefined;
 
-  // Fetch user if filter specifies a user
-  if (filter.user) {
-    user = await userAxiosInstance.get<IUser>(`/users/${filter.user}`);
-  }
+  if (!SupportedType(booking.type)) return null;
 
-  // Fetch entity if filter specifies an entity
-  if (filter.entity) {
-    const entityUrl =
-      filter.type === bookingType.Activity
-        ? `/activities/${filter.entity}`
-        : `/itineraries/${filter.entity}`;
-    entity = await entertainmentAxiosInstance.get<IActivity | IItinerary>(entityUrl);
-    
-  }
+  populatedUser = (!user) ? (await userAxiosInstance.get<IUser>(`/users/${booking.user}`)).data : user;
 
-  if (booking.type === bookingType.Activity || booking.type === bookingType.Itinerary) {
-    
-    if (!filter.user) {
-      user = await userAxiosInstance.get<IUser>(`/users/${booking.user}`);
-    }
-    if (!filter.entity) {
-      const entityUrl =
-        booking.type === bookingType.Activity
-          ? `/activities/${booking.entity}`
-          : `/itineraries/${booking.entity}`;
-      
-      entity = await entertainmentAxiosInstance.get<IActivity | IItinerary>(entityUrl);
-      
-    }
+  populatedEntity = (!entity) ? await populateEntity(booking.entity, booking.type) : entity;
 
-    return {
-      _id: booking._id,
-      user: {
-        _id: user!.data._id,
-        username: user!.data.username,
-        email: user!.data.email,
-        firstName: user!.data.firstName,
-        lastName: user!.data.lastName,
-        phoneNumber: user!.data.phoneNumber,
-        dob: user!.data.dob,
-        nationality: user!.data.nationality,
-      },
-      entity: entity!.data,
-      type: booking.type,
-      status: booking.status ?? bookingStatus.Upcoming,
-      selectedPrice: booking.selectedPrice ?? CONSTANTS.ZERO,
-      selectedDate: booking.selectedDate,
-    };
-  }
-  return null;
-
+  return {
+    _id: booking._id,
+    user: {
+      _id: populatedUser!._id,
+      username: populatedUser!.username,
+      email: populatedUser!.email,
+      firstName: populatedUser!.firstName,
+      lastName: populatedUser!.lastName,
+      phoneNumber: populatedUser!.phoneNumber,
+      dob: populatedUser!.dob,
+      nationality: populatedUser!.nationality,
+    },
+    entity: populatedEntity,
+    type: booking.type,
+    status: booking.status ?? bookingStatus.Upcoming,
+    selectedPrice: booking.selectedPrice ?? 0,
+    selectedDate: booking.selectedDate,
+  };
 }
-
