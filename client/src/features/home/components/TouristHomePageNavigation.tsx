3549b062d6f3eaa02830adeb591d2fce1c85da16
Ali Shokry <amsaar.1416@gmail.com>
2024-11-27 20:11:24 +0200

    feat: add cart icon to navigation bar

--- client/src/features/home/components/TouristHomePageNavigation.tsx
+++ client/src/features/home/components/TouristHomePageNavigation.tsx
@@ -5,6 +5,7 @@ import { Link, useLocation, useParams } from "react-router-dom";
 import { ProfileAvatar } from "./ProfileAvatar";
 import CurrencyDropdown from "./CurrencyDropdown";
 import SecondaryLogo from "../../logos/SecondaryLogo";
+import { CartIcon } from "@/features/checkout/components/CartIcon";
 
 interface ButtonProps {
   navigation: number;
@@ -73,6 +74,7 @@ export default function TouristHomePageNavigation(NavigationProps: NavigationPro
         ) : (
           <>
             <div className="flex space-x-4 items-center">
+              <CartIcon />
               <CurrencyDropdown />
               <ProfileAvatar />
             </div>
