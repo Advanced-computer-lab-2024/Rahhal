06fedd96a4dec5cb2941f25992b8d8e96ab5fc6f
Abdelrahman M.Talaat <abdelrahmanmt96@gmail.com>
2024-11-27 21:46:07 +0200

    api-gateway-feat/add delete bookmark by query endpoint (#213)
    * refactor: reorganize booking and bookmark controllers into a dedicated directory
    
    * refactor: add delete bookmark by query endpoint

--- api-gateway/src/api/routes/booking-routes.ts
+++ api-gateway/src/api/routes/booking-routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import * as bookingController from "@/api/controllers/booking-controller";
-import * as bookmarkController from "@/api/controllers/bookmark-controller";
+import * as bookingController from "@/api/controllers/booking-controllers/booking-controller";
+import * as bookmarkController from "@/api/controllers/booking-controllers/bookmark-controller";
 
 const router = express.Router();
 
@@ -16,6 +16,7 @@ router.get("/bookmarks", bookmarkController.getBookmarks);
 router.get("/bookmarks/:id", bookmarkController.getBookmarkById);
 router.post("/bookmarks", bookmarkController.createBookmark);
 router.patch("/bookmarks/:id", bookmarkController.updateBookmark);
-router.delete("/bookmarks/:id", bookmarkController.deleteBookmark);
+router.delete("/bookmarks", bookmarkController.deleteBookmark);
+router.delete("/bookmarks/:id", bookmarkController.deleteBookmarkById);
 
 export default router;
