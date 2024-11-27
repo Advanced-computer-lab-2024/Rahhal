1a61596ac945cf1d8fe29844d5adfdec925dfb67
Amr Hegazy <68474553+Amr-Hegazy1@users.noreply.github.com>
2024-11-27 22:29:47 +0200

    feat: backend filtering in `ProductReport`

--- client/src/features/seller/components/ProductReport.tsx
+++ client/src/features/seller/components/ProductReport.tsx
@@ -1,44 +1,65 @@
 import { useEffect, useState } from "react";
-import GenericSalesReport, { ReportFilters, SalesItem } from "../../../components/GenericSalesReport";
+import GenericSalesReport, {
+  ReportFilters,
+  SalesItem,
+} from "../../../components/GenericSalesReport";
 import { useParams } from "react-router-dom";
 import { TOrder } from "@/types/shared";
-import { fetchProductOrders } from "@/api-calls/order-api-calls";
+import { fetchOrdersByDateRange, fetchProductOrders } from "@/api-calls/order-api-calls";
 
 export default function ProductReport() {
   const [salesData, setSalesData] = useState<SalesItem[]>([]);
 
-  const { id: productId } = useParams<{ id: string }>();
-  
-  
-  useEffect(() => {
+  const [filters, setFilters] = useState<ReportFilters | null>(null);
 
-    
-    
+  const { id: productId } = useParams<{ id: string }>();
+
+  useEffect(() => {
     // fetch orders for this product
     if (!productId) return;
-    fetchProductOrders(productId).then((value) => {
-      const orders = value as TOrder[];
-      
-      const salesItems: SalesItem[] = orders.map(order => ({
-        id: order._id,
-        name: order.items[0].name,
-        type: 'gift_shop',
-        price: order.totalPrice,
-        date: order.createdAt.toISOString(),
-        quantity: order.totalQuantity,
-        status: order.orderStatus
-      }));
-      setSalesData(salesItems);
-      
-    });
+    if (!filters) {
+      fetchProductOrders(productId).then((value) => {
+        const orders = value as TOrder[];
 
-  }, []);
+        const salesItems: SalesItem[] = orders.map((order) => ({
+          id: order._id,
+          name: order.items[0].name,
+          type: "gift_shop",
+          price: order.totalPrice,
+          date: new Date(order.createdAt).toISOString(),
+          quantity: order.totalQuantity,
+          status: order.orderStatus,
+        }));
+        setSalesData(salesItems);
+      });
+    } else {
+      const startDate = filters.dateRange[0];
+      const endDate = filters.dateRange[1];
+      
+
+      fetchOrdersByDateRange(startDate, endDate).then((value) => {
+        const orders = value as TOrder[];
+        console.log("Orders", orders);
+
+        const salesItems: SalesItem[] = orders.map((order) => ({
+          id: order._id,
+          name: order.items[0].name,
+          type: "gift_shop",
+          price: order.totalPrice,
+          date: new Date(order.createdAt).toISOString(),
+          quantity: order.totalQuantity,
+          status: order.orderStatus,
+        }));
+        setSalesData(salesItems);
+      });
+    }
+  }, [productId, filters]);
 
   return (
     <GenericSalesReport
       data={salesData}
       type="gift_shop"
-      onFilterChange={(filters: ReportFilters) => console.log("Filters changed:", filters)}
+      onFilterChange={(filters: ReportFilters) => setFilters(filters)}
     />
   );
 }
