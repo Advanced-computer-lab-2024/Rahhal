133cb479b7fa5455a824808763a588ff8cd38af0
Ali Shokry <125921562+AliShokryy@users.noreply.github.com>
2024-11-28 21:24:36 +0200

    Refactored user endpoints (#196)
    * feat: add deactivate activities or itenraries if it has bookings and account requested to be deleted
    
    * refactor: refactored user endpoint methods and removed unnecessary methods
    
    * fix: return user Data after redeem
    
    * refactor: removed repeated functions and updated them to be one function
    
    * fix: add get all users function

--- user/src/database/repositories/user-repository.ts
+++ user/src/database/repositories/user-repository.ts
@@ -2,37 +2,15 @@ import type { TRating } from "@/types";
 import type { IUser } from "../models/User";
 import User from "../models/User";
 
-//get specific user
-export async function getUserByUsername(username: string): Promise<IUser | null> {
-  return await User.findOne({ username: username , deleted:false });
+export async function getAllUsers(filter: Partial<IUser>) {
+  return await User.find({deleted: false , ...filter});
 }
 
-export async function getUserByEmail(email: string): Promise<IUser | null> {
-  return await User.findOne({ email: email , deleted:false });
+export async function getUser(filter: Partial<IUser>){
+  return await User.findOne({deleted: false , ...filter});
 }
 
-export async function getUserById(userId: string): Promise<IUser | null> {
-  return await User.findById(userId,{deleted:false});
-}
-
-//get all users or filter them based on given parameters
-export async function getAllUsers(approved?: boolean): Promise<IUser[] | null> {
-  return await User.find(approved !== undefined ? { approved: approved , deleted:false} : {});
-}
-
-//update user
-export async function updateUserByUsername(
-  username: string,
-  updatedUser: IUser,
-): Promise<IUser | null> {
-  return await User.findOneAndUpdate({ username: username }, updatedUser, {
-    new: true,
-    runValidators: true,
-  });
-}
-
-//update user by id
-export async function updateUserById(userId: string, updatedUser: IUser): Promise<IUser | null> {
+export async function updateUser(userId: string, updatedUser: IUser): Promise<IUser | null> {
   return await User.findByIdAndUpdate(userId, updatedUser, { new: true, runValidators: true });
 }
 
--- user/src/services/user-service.ts
+++ user/src/services/user-service.ts
@@ -3,17 +3,17 @@ import {type IUser } from "../database/models/User";
 import { bookingType } from "@/utils/constants";
 import type { TRating } from "@/types";
 import { Role } from "@/utils/constants";
+import { ObjectId } from "mongodb";
 import  { bookingStatus , POINTS , LEVELS} from "@/utils/constants";
-import { hasBookings , deleteEntities } from "@/services/booking-calls";
+import { hasBookings , deleteEntities, deactivateEntities } from "@/services/booking-calls";
 
 
 export async function createUser(userData: IUser) {
   return await userRepository.createUser(userData);
 }
 
-//check if the user already exists
 export async function deleteUser(userId: string) {
-  const user = await userRepository.getUserById(userId);
+  const user = await userRepository.getUser({_id : new ObjectId(userId)});
   if(!user){
     throw new Error("User not found");
   }
@@ -28,12 +28,13 @@ export async function deleteUser(userId: string) {
       let errorMessage = "";
       switch(user.role){
         case Role.tourGuide:
-          errorMessage = "Tour guide has upcoming itineraries that have bookings";
+          errorMessage = "You have upcoming itineraries that have bookings, Itenraries will be deactivated";
           break;
         case Role.advertiser:
-          errorMessage = "Advertiser has upcoming activities that have bookings";
+          errorMessage = "You have upcoming activities that have bookings, Activities will be deactivated";
           break;
       }
+      deactivateEntities({owner: userId , role: user.role});
       throw new Error(errorMessage);
     }
   }
@@ -42,37 +43,16 @@ export async function deleteUser(userId: string) {
   return await userRepository.deleteUser(userId);
 }
 
-//get specific user by username
-export async function getUserByUsername(username: string): Promise<IUser | null> {
-  return await userRepository.getUserByUsername(username);
+export async function getAllUsers(filter: Partial<IUser>) {
+  return await userRepository.getAllUsers(filter);
 }
 
-//get specific user by id
-export async function getUserById(userId: string): Promise<IUser | null> {
-  return await userRepository.getUserById(userId);
+export async function getUser(filter: Partial<IUser>): Promise<IUser | null> {
+  return await userRepository.getUser(filter);
 }
 
-//get specific user by email
-export async function getUserByEmail(email: string): Promise<IUser | null> {
-  return await userRepository.getUserByEmail(email);
-}
-
-//get all users
-export async function getAllUsers(approved?: boolean): Promise<IUser[] | null> {
-  return await userRepository.getAllUsers(approved);
-}
-
-//update user and searching for the user using it username
-export async function updateUserByUsername(
-  username: string,
-  updatedUser: IUser,
-): Promise<IUser | null> {
-  return await userRepository.updateUserByUsername(username, updatedUser);
-}
-
-//update user and searching for the user using it id
-export async function updateUserById(userId: string, updatedUser: IUser): Promise<IUser | null> {
-  return await userRepository.updateUserById(userId, updatedUser);
+export async function updateUser(userId: string, updatedUser: IUser): Promise<IUser | null> {
+  return await userRepository.updateUser(userId, updatedUser);
 }
 
 export async function addRating(userRating: TRating, ratedUserId: string) {
@@ -85,7 +65,7 @@ export async function redeemPoints(user:IUser){
   updatedUser.points = user.points as number % POINTS.MINPOINTS;
   updatedUser.balance =
     (user.balance as number) + Math.floor(avgBalance) * POINTS.AMOUNTFORMINPOINTS;
-  const newUser = await userRepository.updateUserById(user._id.toString() , updatedUser);
+  const newUser = await userRepository.updateUser(user._id.toString() , updatedUser);
   return newUser;
 }
 
