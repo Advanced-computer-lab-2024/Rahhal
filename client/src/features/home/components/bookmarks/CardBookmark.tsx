86e9ac966668fe4148a6158a75b7a36e3aa0d463
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-11-29 12:01:52 +0200

    feat: show SignUpModal for guest users when attempting to bookmark an entity

--- client/src/features/home/components/bookmarks/CardBookmark.tsx
+++ client/src/features/home/components/bookmarks/CardBookmark.tsx
@@ -1,35 +1,38 @@
 "use client";
 import * as React from "react";
-import { Book, Bookmark, Heart } from "lucide-react";
+import { Bookmark } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useEffect, useState } from "react";
 import useBookmarkStore from "@/stores/bookmark-count-store";
 import { addBookmark, isEntityBookmarked, removeBookmark } from "@/api-calls/bookmark-api-calls";
 import { useParams } from "react-router-dom";
 import { bookmarkType } from "@/utils/enums";
+import SignUpModal from "../SignupModal";
+import { toast } from "@/hooks/use-toast";
 
 interface BookmarkButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   id: string;
   entityType: bookmarkType;
 }
 
-export default function CardBookmark({ id, entityType, className, ...props }: BookmarkButtonProps) {
+export default function CardBookmark({ id, entityType }: BookmarkButtonProps) {
   const { id: userId } = useParams();
   const [isBookmarked, setBookmarked] = useState(false);
   const { incrementCount, decrementCount } = useBookmarkStore();
+  const [isGuestAction, setIsGuestAction] = useState(false);
 
   useEffect(() => {
     if (!userId) return;
     isEntityBookmarked(userId, id, entityType).then(setBookmarked);
-    console.log("Wishlist status", isBookmarked);
   }, [userId, id]);
 
   const handleBookmarkClick = async (e: React.MouseEvent) => {
     e.stopPropagation();
-    if (!id) return; // Ensure the product has a valid ID
+
+    if (!id) return;
 
     if (!userId) {
-      alert("You must login");
+      setIsGuestAction(true);
       return;
     }
 
@@ -39,7 +42,11 @@ export default function CardBookmark({ id, entityType, className, ...props }: Bo
         setBookmarked(false);
         decrementCount();
       } catch (error) {
-        console.error("Failed to remove from wishlist", error);
+        toast({
+          title: `Failed to remove ${entityType} bookmark, Please try again later.`,
+          variant: "destructive",
+          duration: 3500,
+        });
       }
     } else {
       try {
@@ -47,21 +54,36 @@ export default function CardBookmark({ id, entityType, className, ...props }: Bo
         setBookmarked(true);
         incrementCount();
       } catch (error) {
-        console.error("Failed to add to wishlist", error);
+        toast({
+          title: `Failed to add ${entityType} bookmark, Please try again later.`,
+          variant: "destructive",
+          duration: 3500,
+        });
       }
     }
   };
   return (
-    <Bookmark
-      className={cn(
-        "absolute top-2 right-2",
-        "transition-all duration-200",
-        "hover:scale-110 active:scale-95",
-        "stroke-[1.5]",
-        "stroke-white",
-        isBookmarked ? "fill-[var(--primary-color)]" : " fill-foreground/50",
+    <>
+      {isGuestAction && (
+        <SignUpModal
+          onClose={(e) => {
+            e.stopPropagation();
+            setIsGuestAction(false);
+          }}
+          text={"Making a list? Sign in to bookmark your favourite events"}
+        />
       )}
-      onClick={handleBookmarkClick}
-    />
+      <Bookmark
+        className={cn(
+          "absolute top-2 right-2",
+          "transition-all duration-200",
+          "hover:scale-110 active:scale-95",
+          "stroke-[1.5]",
+          "stroke-white",
+          isBookmarked ? "fill-[var(--primary-color)]" : " fill-foreground/50",
+        )}
+        onClick={handleBookmarkClick}
+      />
+    </>
   );
 }
