bd7a95049487ae974ae4a88c75bcf089fdce06e4
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-11-30 02:22:02 +0200

    feat: enhance OverViewButton with notification and animation on click

--- client/src/features/home/components/overview-card/OverViewButton.tsx
+++ client/src/features/home/components/overview-card/OverViewButton.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import ButtonStyles from "../../styles/Overview-card-styles/OverViewButton.module.css";
 import AnimatedNotificationBell from "../NotificationBell";
+import { toast } from "@/hooks/use-toast";
 interface ButtonProps {
   label: string;
   onClick?: () => void;
@@ -8,13 +10,25 @@ interface ButtonProps {
 }
 
 export const OverViewButton: React.FC<ButtonProps> = ({ label, onClick, color, disabled }) => {
+
+  const [isAnimating, setIsAnimating] = useState(false);
+  const handleButtonClick = () => {
+    setIsAnimating(true);
+    //Todo add notify me logic here
+    setTimeout(() => setIsAnimating(false), 1000);
+
+    toast({
+      title: `You will be notified when activity is available`,
+      duration: 3500,
+    });
+  }
   return (
     <button
       className={`${ButtonStyles.button} ${ButtonStyles[color]} ${disabled ? ButtonStyles.disabled : ""}`}
-      onClick={onClick}
+      onClick={color==="blue"?handleButtonClick: onClick}
       disabled={disabled}
     >
-      {color === "blue" && <AnimatedNotificationBell color={"white"} />}
+      {color === "blue" && <AnimatedNotificationBell color={"white"} animate={isAnimating} />}
       {label}
     </button>
   );
