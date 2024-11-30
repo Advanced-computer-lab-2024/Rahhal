8f3380cf6171593cd36892542b17655e6c3fc3e5
Abdelrahman M.Talaat <abdelrahmanmt96@gmail.com>
2024-11-30 20:50:58 +0200

    feat: implement RabbitMQ notification and event reminder publishers (#227)

--- entertainment/src/services/activities-service.ts
+++ entertainment/src/services/activities-service.ts
@@ -3,6 +3,7 @@ import type { IRating } from "@/database/shared";
 import * as activitiesRepository from "../database/repositories/activities-repository";
 import { hasBookings } from "@/utils/booking-axios-instance";
 import { bookingType } from "@/utils/constants";
+import { publishNotification } from "@/publishers/notification-publisher";
 
 // Get all activities
 export async function getAllActivities(filter: Partial<IActivity>) {
@@ -30,6 +31,9 @@ export async function addRating(userRating: IRating, activityId: string) {
 
 // Update an existing activity
 export async function updateActivity(id: string, activitiesData: IActivity) {
+  if (activitiesData.isAppropriate === false) {
+    await sendMarkedInappropriateNotification(id);
+  }
   return activitiesRepository.updateActivity(id, activitiesData);
 }
 
@@ -40,5 +44,19 @@ export async function deleteActivity(id: string) {
     throw new Error("Cannot delete activity with bookings");
   } else {
     return activitiesRepository.deleteActivity(id);
+  }
+}
+
+export async function sendMarkedInappropriateNotification(activityId: string) {
+  const activity = await activitiesRepository.getActivityById(activityId);
+  if (activity) {
+    const data = {
+      userId: activity.owner,
+      message: `Hello Dear User, \nYour Activity : \n Name: ${activity.name} with ID ${activity._id} \nhas been marked inappropriate \nPlease review and take necessary action \n\nRegards, \nRahhal Team`,
+    };
+
+    await publishNotification(data);
+  } else {
+    throw new Error("Activity not found");
   }
 }
