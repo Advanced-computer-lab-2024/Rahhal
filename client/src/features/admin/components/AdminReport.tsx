327fd105d05ae095315fad130cf867bb5c65b6ab
Amr Hegazy <68474553+Amr-Hegazy1@users.noreply.github.com>
2024-12-01 21:35:07 +0200

    fix: fix AdminReport to include fetched products and update sales data logic

--- client/src/features/admin/components/AdminReport.tsx
+++ client/src/features/admin/components/AdminReport.tsx
@@ -4,17 +4,13 @@ import GenericSalesReport, {
   SalesItem,
 } from "../../../components/GenericSalesReport";
 
-import {
-  fetchBookingsByDateRange,
-  getBookingsWithFilters,
-} from "@/api-calls/booking-api-calls";
+import { fetchBookingsByDateRange, getBookingsWithFilters } from "@/api-calls/booking-api-calls";
 import { TOrder, TPopulatedBooking } from "@/features/home/types/home-page-types";
-import {
-  fetchAllOrders,
-  fetchOrdersByDateRange,
-} from "@/api-calls/order-api-calls";
+import { fetchAllOrders, fetchOrdersByDateRange } from "@/api-calls/order-api-calls";
 import { getAdmins } from "@/api-calls/users-api-calls";
 import { TUser } from "@/types/user";
+import { fetchProducts } from "@/api-calls/products-api-calls";
+import { TProduct } from "@/features/seller/utils/seller-columns";
 
 export default function AdminReport() {
   const [salesData, setSalesData] = useState<SalesItem[]>([]);
@@ -28,29 +24,23 @@ export default function AdminReport() {
       const itineraryFilters = { type: "itinerary", status: "completed" };
       const activityFilters = { type: "activity", status: "completed" };
 
-      const [bookingsResult, activitiesResult, ordersResult] = await Promise.all([
+      const [bookingsResult, activitiesResult, ordersResult, productsResult] = await Promise.all([
         filters
-          ? fetchBookingsByDateRange(
-              filters.dateRange[0],
-              filters.dateRange[1],
-              itineraryFilters
-            )
+          ? fetchBookingsByDateRange(filters.dateRange[0], filters.dateRange[1], itineraryFilters)
           : getBookingsWithFilters(itineraryFilters),
         filters
-          ? fetchBookingsByDateRange(
-              filters.dateRange[0],
-              filters.dateRange[1],
-              activityFilters
-            )
+          ? fetchBookingsByDateRange(filters.dateRange[0], filters.dateRange[1], activityFilters)
           : getBookingsWithFilters(activityFilters),
         filters
           ? fetchOrdersByDateRange(filters.dateRange[0], filters.dateRange[1])
           : fetchAllOrders(),
+        fetchProducts(),
       ]);
 
       const bookings = bookingsResult as TPopulatedBooking[];
       const activities = activitiesResult as TPopulatedBooking[];
       const orders = ordersResult as TOrder[];
+      const products = productsResult as TProduct[];
 
       const salesItems: SalesItem[] = [
         ...bookings.map((booking) => ({
@@ -73,21 +63,33 @@ export default function AdminReport() {
           status: booking.status,
           tourists: 1,
         })),
-        ...orders
-          .flatMap((order) =>
+        ...orders.flatMap(
+          (order) =>
             order.items.map((product) => ({
               id: product.productId,
               name: product.name,
               type: "gift_shop",
-              price: adminIds.includes(product.seller)
-                ? product.price
-                : product.price * 0.1,
+              price: adminIds.includes(product.seller) ? product.price : product.price * 0.1,
               date: order.createdAt,
               quantity: product.quantity,
               status: order.orderStatus,
               tourists: 0,
-            }))
-          ),
+            })),
+
+          // fetch rest of products that are not in orders
+          ...products
+            .filter((product) => !salesData.find((item) => item.id === product._id))
+            .map((product) => ({
+              id: product._id,
+              name: product.name,
+              type: "gift_shop",
+              price: product.price,
+              date: new Date().toISOString(),
+              quantity: 0,
+              status: "not_sold",
+              tourists: 0,
+            })),
+        ),
       ];
 
       setSalesData(salesItems);
