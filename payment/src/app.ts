e0e1fbe14d8537ce943fdb2b9ef2413f43018932
Ahmed Hawater <hawatercoding@gmail.com>
2024-12-02 22:47:03 +0200

    feat: Add initial stripe backend to payment service

--- payment/src/app.ts
+++ payment/src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
 import promocodeRoutes from "./api/routes/promocode-routes";
 
 const app = express();
 
+// Middleware
 app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
+// Routes
 app.use("/promocode", promocodeRoutes)
 
 export default app;
