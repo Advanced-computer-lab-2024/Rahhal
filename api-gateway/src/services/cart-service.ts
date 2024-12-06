81dd7398644f7de96402c1260417f8b45d61d822
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-12-06 09:51:36 +0200

    feat: enhance cart functionality with add and remove item endpoints, and update cart structure

--- api-gateway/src/services/cart-service.ts
+++ api-gateway/src/services/cart-service.ts
@@ -1,10 +1,12 @@
 import { orderAxiosInstance } from "@/utils/axios-instances";
 import { populateCart } from "@/utils/cart-populator";
-import type { ICart } from "@/utils/types";
+import type { CartUpdates } from "@/utils/types";
 
-export async function getCart(userId: string) {
+export async function getCart(user: string) {
   try {
-    const cart = await orderAxiosInstance.get("/carts?userId=" + userId);
+    const cart = await orderAxiosInstance.get("/carts", {
+      params: { user },
+    });
     return await populateCart(cart.data);
   } catch (error) {
     return new Error("Failed to fetch cart from order server\n" + error);
@@ -15,19 +17,43 @@ export async function createCart(body: string) {
   return orderAxiosInstance.post("/carts", body);
 }
 
-export async function updateCart(userId: string, updatedCart: Partial<ICart>) {
+export async function updateCart(cart: {
+  user: string;
+  operation: CartUpdates;
+  productId: string;
+}) {
   try {
-    const updatedCartResponse = await orderAxiosInstance.patch(
-      `/carts?userId=${userId}`,
-      updatedCart,
-    );
-    const populatedCart = await populateCart(updatedCartResponse.data);
-
-    return populatedCart;
+    const updatedCartResponse = await orderAxiosInstance.patch("/carts", null, {
+      params: cart,
+    });
+    return await populateCart(updatedCartResponse.data);
   } catch (error) {
     throw new Error("Failed to update the cart\n" + error);
   }
 }
-export async function deleteCart(id: string) {
-  return orderAxiosInstance.delete(`/carts?userId=${id}`);
+
+export async function addItemToCart(body: string) {
+  try {
+    const populatedCart = await orderAxiosInstance.post("/carts/add", body);
+    return await populateCart(populatedCart.data);
+  } catch (error) {
+    throw new Error("Failed to add item to cart\n" + error);
+  }
+}
+
+export async function removeItemFromCart(user: string, productId: string) {
+  try {
+    const populatedCart = await orderAxiosInstance.delete("/carts/remove", {
+      params: { user: user, productId: productId },
+    });
+    return await populateCart(populatedCart.data);
+  } catch (error) {
+    throw new Error("Failed to remove item from cart\n" + error);
+  }
+}
+
+export async function deleteCart(user: string) {
+  return orderAxiosInstance.delete("/carts", {
+    params: { user },
+  });
 }
