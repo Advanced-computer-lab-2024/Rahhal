69ae8ddf75f03908681f6fcaadf41702835103ba
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-12-07 20:54:48 +0200

    fix: added back currency dropdown

--- client/src/features/home/components/TouristHomePageNavigation.tsx
+++ client/src/features/home/components/TouristHomePageNavigation.tsx
@@ -101,7 +101,7 @@ export default function TouristHomePageNavigation(NavigationProps: NavigationPro
                   </div>
                 </>
               ) : null}
-
+              <CurrencyDropdown />
               <ProfileAvatar />
             </div>
           </>
