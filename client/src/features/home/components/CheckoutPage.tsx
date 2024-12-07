364f0be89ef7ba5873418a1b046856b173ebcfd7
Abdallah Ahmed <101679255+AbdallahAhmadd@users.noreply.github.com>
2024-12-07 08:27:01 +0200

    feat: integrate TouristHomePageNavigation and useParams in CheckoutPage (#259)

--- client/src/features/home/components/CheckoutPage.tsx
+++ client/src/features/home/components/CheckoutPage.tsx
@@ -1,11 +1,14 @@
 import CheckoutNavigation from "./CheckoutNavigation";
-import { Outlet } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
+import TouristHomePageNavigation from "./TouristHomePageNavigation";
+
 
 
 export default function CheckoutPage() {
+  const { id } = useParams<{ id?: string }>();
   return (
     <>
-      <CheckoutNavigation/>
+      <TouristHomePageNavigation loggedIn={id? true:false }/>
       <Outlet />
     </>
   );
