39794bd41dbd19976383a4bdf2b13758701c0234
Abdallah Ahmed <101679255+AbdallahAhmadd@users.noreply.github.com>
2024-12-09 01:58:29 +0200

    change complaints table colors (#305)
    * refactor: improve layout and styling of TouristComplaintsTable component
    
    * style: update button styling in TouristComplaintsTable component

--- client/src/features/user-settings/components/TouristComplaintsTable.tsx
+++ client/src/features/user-settings/components/TouristComplaintsTable.tsx
@@ -22,7 +22,7 @@ function ComplaintsTable() {
   }, [id]);
 
   const handleCreateComplaint = async (newComplaint: TComplaint) => {
-    const complaint = await createComplaint(newComplaint) as TComplaint;
+    const complaint = (await createComplaint(newComplaint)) as TComplaint;
     toast({
       title: "Success",
       description: "Complaint Created Successfully",
@@ -33,46 +33,44 @@ function ComplaintsTable() {
 
   return (
     <div className="p-4">
-    {complaints.length === 0 ? (
+      {complaints.length === 0 ? (
         <div className="flex flex-col items-center">
-          <img
-            src={NoComplaints}
-            alt="No Complaints Yet"
-            className="w-1/2 h-auto mb-4"
-          />
+          <img src={NoComplaints} alt="No Complaints Yet" className="w-1/2 h-auto mb-4" />
           <p className="text-lg text-gray-600 mb-4">
-          You haven't filed any complaints yet. If you're experiencing any issues, feel free to let us know by clicking the button below!
+            You haven't filed any complaints yet. If you're experiencing any issues, feel free to
+            let us know by clicking the button below!
           </p>
           {/* Button for adding a complaint */}
           <TouristComplaintsModal
             complaintData={undefined}
-            dialogTrigger={<DataTableAddButton className="bg-[#1d3c51] text-white rounded"/>}
+            dialogTrigger={<DataTableAddButton className="bg-[var(--primary-color)] hover:bg-[var(--primary-color-hover)] text-white rounded" />}
             id={id ? id : ""}
             onCreateComplaint={handleCreateComplaint}
           />
         </div>
       ) : (
         <div className="bg-white rounded-lg shadow-lg border border-gray-300 overflow-hidden">
-        <div className="p-4 border-b border-gray-300">
-          <div style={{ backgroundColor: '#1d3c51', padding: '10px', borderRadius: '5px' }}>
-            <h2 className="text-lg font-semibold" style={{ color: 'white' }}>My Complaints</h2>
+          <div className="p-4 border-b border-gray-300">
+            <div style={{ backgroundColor: "#6d91e1", padding: "10px", borderRadius: "5px" }}>
+              <h2 className="text-lg font-semibold" style={{ color: "white" }}>
+                My Complaints
+              </h2>
+            </div>
+            <div className="flex justify-between border-b border-gray-300 pb-2 mb-4"></div>
+            <DataTable
+              data={complaints}
+              columns={touristComplaintsColumns}
+              newRowModal={
+                <TouristComplaintsModal
+                  complaintData={undefined}
+                  dialogTrigger={<DataTableAddButton className="bg-[var(--complimentary-color)] hover:bg-[var(--complimentary-color-dark)] text-white rounded" />}
+                  id={id ? id : ""}
+                  onCreateComplaint={handleCreateComplaint}
+                />
+              }
+            />
           </div>
-          <div className="flex justify-between border-b border-gray-300 pb-2 mb-4">
-          </div>
-          <DataTable
-            data={complaints}
-            columns={touristComplaintsColumns}
-            newRowModal={
-              <TouristComplaintsModal
-                complaintData={undefined}
-                dialogTrigger={<DataTableAddButton className="bg-[#1d3c51] text-white rounded" />}
-                id={id ? id : ""}
-                onCreateComplaint={handleCreateComplaint}
-              />
-            }
-          />
         </div>
-      </div>
       )}
     </div>
   );
