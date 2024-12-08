8bd41c7c01033120d1492bd39a8891d4da598e7f
YousefElbrolosy <yousefelbrolosy@gmail.com>
2024-12-08 06:08:56 +0200

    fix: adjust alignment for settings for non tourist

--- client/src/features/user-settings/components/ProfileForm.tsx
+++ client/src/features/user-settings/components/ProfileForm.tsx
@@ -199,7 +199,7 @@ export default function ProfileForm() {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <div className="space-y-6" style={{ width: "80%" }}>
+        <div className="space-y-6" style={{ width: user.role === "tourist" ? "80%" : "100%" }}>
           <div className="flex items-center justify-between">
             <h3 className="text-lg font-medium">Profile</h3>
             <Button
