37a572550ef8e2f48cfd5eb8441c43ab586704b3
Amr Hegazy <68474553+Amr-Hegazy1@users.noreply.github.com>
2024-12-08 03:55:29 +0200

    feat: add filter options for trips in MyTripsPage component

--- client/src/features/home/components/my-trips-page/MyTripsPage.tsx
+++ client/src/features/home/components/my-trips-page/MyTripsPage.tsx
@@ -8,6 +8,44 @@ import { bookingType } from "@/utils/enums";
 import { toast } from "@/hooks/use-toast";
 import luggage from "@/assets/luggage.svg";
 import EmptyStatePlaceholder from "../EmptyStatePlaceholder";
+import { AiOutlineThunderbolt } from "react-icons/ai";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { MdLocalHotel, MdOutlineEmojiTransportation } from "react-icons/md";
+import { useEffect, useState } from "react";
+
+const transferOptions = [
+  {
+    type: "experiences",
+    label: "Experiences",
+    icon: <AiOutlineThunderbolt className="mr-2" size={20} />,
+    dropdownItems: [
+      { label: "Activity", value: "activity" },
+      { label: "Itinerary", value: "itinerary" },
+    ],
+  },
+  {
+    type: "stays",
+    label: "Stays",
+    icon: <MdLocalHotel className="mr-2" size={20} />,
+    dropdownItems: [{ label: "Hotel", value: "hotel" }],
+  },
+  {
+    type: "travel",
+    label: "Travel",
+    icon: <MdOutlineEmojiTransportation className="mr-2" size={20} />,
+    dropdownItems: [
+      { label: "Flight", value: "flight" },
+      { label: "Taxi", value: "transportation" },
+      { label: "Bus", value: "bus" },
+    ],
+  },
+];
 
 export const MyTripsPage = () => {
   function formatDate(dateString: string | Date): string {
@@ -67,33 +105,73 @@ export const MyTripsPage = () => {
     return navigate(`/my-trips-details/${userId}`, { state: { booking } });
   };
 
+  const [selectedMainFilter, setSelectedMainFilter] = useState("experiences");
+  const [selectedSubFilter, setSelectedSubFilter] = useState("activity");
+
+  useEffect(() => {
+    
+    // update the main filter when the sub filter changes
+    const selectedOption = transferOptions.find((option) =>
+      option.dropdownItems.some((item) => item.value === selectedSubFilter),
+    );
+    if (selectedOption) setSelectedMainFilter(selectedOption.type);
+
+
+  }, [selectedSubFilter]);
+
   return (
     <div className="mb-5">
       <div className={PageStyles["trip-page-header"]}>
         <p>Trips & Booking</p>
+        <div className="flex justify-end px-16 py-[1%] space-x-2">
+        {transferOptions.map((option) => (
+          <DropdownMenu key={option.type}>
+            <DropdownMenuTrigger asChild>
+              <Button
+                className={`rounded-full bg-transparent text-black hover:bg-gray-200 px-6 py-3 text-md ${selectedMainFilter === option.type ? "bg-gray-200" : ""}`}
+                
+              >
+                {option.icon}
+                {option.label}
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              {option.dropdownItems.map((item) => (
+                <DropdownMenuItem key={item.value} onClick={() => setSelectedSubFilter(item.value)}>
+                  {item.label}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ))}
       </div>
+      </div>
+      
       <div className={PageStyles["trip-list"]}>
         {booking && booking.length > 0
-          ? booking.map((booking: TPopulatedBooking) => (
-              <MyTripsCard
-                key={booking.entity._id}
-                title={booking.entity.name || `${booking.type}`}
-                price={booking.selectedPrice}
-                status={
-                  isDateInPast(
-                    booking.selectedDate ? booking.selectedDate : booking.selectedDate,
-                    booking.status,
-                  )
-                    ? "completed"
-                    : booking.status
-                }
-                date={formatDate(
-                  booking.selectedDate ? booking.selectedDate : booking.selectedDate,
-                )}
-                image={booking.entity.images ? booking.entity.images[0] : undefined}
-                onClick={() => handleClick(booking)}
-              />
-            ))
+          ? booking.map(
+              (booking: TPopulatedBooking) =>
+                booking.type === selectedSubFilter && (
+                  <MyTripsCard
+                    key={booking.entity._id}
+                    title={booking.entity.name || `${booking.type}`}
+                    price={booking.selectedPrice}
+                    status={
+                      isDateInPast(
+                        booking.selectedDate ? booking.selectedDate : booking.selectedDate,
+                        booking.status,
+                      )
+                        ? "completed"
+                        : booking.status
+                    }
+                    date={formatDate(
+                      booking.selectedDate ? booking.selectedDate : booking.selectedDate,
+                    )}
+                    image={booking.entity.images ? booking.entity.images[0] : undefined}
+                    onClick={() => handleClick(booking)}
+                  />
+                ),
+            )
           : !isLoading &&
             !isError && (
               <EmptyStatePlaceholder
